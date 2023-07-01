1d80ac451c4a7de771e2ddae6a6867eef5d75c1a
Kyle Barron <kylebarron2@gmail.com>
2023-06-30 21:42:46 -0400

    Add in geo algs from geopolars/geopolars (#6)
    * area, centroid
    
    * envelope, is_empty, length, simplify
    
    * convex hull
    
    * fmt

---
+++ src/algorithm/geo/mod.rs
@@ -0,0 +1,18 @@
+//! Algorithms implemented on GeoArrow arrays using georust/geo algorithms.
+
+mod area;
+mod centroid;
+mod convex_hull;
+mod envelope;
+mod is_empty;
+mod length;
+mod simplify;
+
+pub use area::area;
+pub use area::signed_area;
+pub use centroid::centroid;
+pub use convex_hull::convex_hull;
+pub use envelope::envelope;
+pub use is_empty::is_empty;
+pub use length::{euclidean_length, geodesic_length, haversine_length, vincenty_length};
+pub use simplify::simplify;
--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,1 +1,2 @@
 pub mod bounding_rect;
+pub mod geo;
