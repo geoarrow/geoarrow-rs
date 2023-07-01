afc83d2d096365d4bd8fa3594a206de953e2f176
Kyle Barron <kylebarron2@gmail.com>
2023-06-30 21:56:05 -0400

    Add more area functions (#7)
    * Add more area functions
    
    * fmt

--- src/algorithm/geo/mod.rs
+++ src/algorithm/geo/mod.rs
@@ -2,8 +2,10 @@
 
 mod area;
 mod centroid;
+mod chamberlain_duquette_area;
 mod convex_hull;
 mod envelope;
+mod geodesic_area;
 mod is_empty;
 mod length;
 mod simplify;
@@ -11,8 +13,12 @@ mod simplify;
 pub use area::area;
 pub use area::signed_area;
 pub use centroid::centroid;
+pub use chamberlain_duquette_area::{
+    chamberlain_duquette_signed_area, chamberlain_duquette_unsigned_area,
+};
 pub use convex_hull::convex_hull;
 pub use envelope::envelope;
+pub use geodesic_area::{geodesic_area_signed, geodesic_area_unsigned, geodesic_perimeter};
 pub use is_empty::is_empty;
 pub use length::{euclidean_length, geodesic_length, haversine_length, vincenty_length};
 pub use simplify::simplify;
