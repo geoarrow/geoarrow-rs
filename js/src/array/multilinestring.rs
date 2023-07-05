06d6de6b2d77fb07ad06cff937431b27c9668fcc
Kyle Barron <kylebarron2@gmail.com>
2023-07-05 11:32:41 -0400

    Implement constructors

--- js/src/array/multilinestring.rs
+++ js/src/array/multilinestring.rs
@@ -3,8 +3,10 @@ use crate::array::point::PointArray;
 use crate::array::polygon::PolygonArray;
 use crate::array::primitive::BooleanArray;
 use crate::array::primitive::Float64Array;
+use crate::array::CoordBuffer;
 use crate::error::WasmResult;
 use crate::impl_geometry_array;
+use crate::utils::vec_to_offsets;
 use arrow2::datatypes::Field;
 use geoarrow::GeometryArrayTrait;
 use wasm_bindgen::prelude::*;
@@ -12,10 +14,27 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub struct MultiLineStringArray(pub(crate) geoarrow::array::MultiLineStringArray);
 
+impl_geometry_array!(MultiLineStringArray);
+
+#[wasm_bindgen]
+impl MultiLineStringArray {
+    #[wasm_bindgen(constructor)]
+    pub fn new(
+        coords: CoordBuffer,
+        geom_offsets: Vec<i32>,
+        ring_offsets: Vec<i32>,
+        validity: Option<BooleanArray>,
+    ) -> Self {
+        Self(geoarrow::array::MultiLineStringArray::new(
+            coords.0,
+            vec_to_offsets(geom_offsets),
+            vec_to_offsets(ring_offsets),
+            validity.map(|validity| validity.0.values().clone()),
+        ))
+    }
+}
 impl From<&MultiLineStringArray> for geoarrow::array::GeometryArray {
     fn from(value: &MultiLineStringArray) -> Self {
         geoarrow::array::GeometryArray::MultiLineString(value.0.clone())
     }
 }
-
-impl_geometry_array!(MultiLineStringArray);
