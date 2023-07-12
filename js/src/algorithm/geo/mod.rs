69234982735a5cabe0934ae94d71cdd07a57691a
Kyle Barron <kylebarron2@gmail.com>
2023-07-12 11:39:11 -0400

    fmt

--- js/src/algorithm/geo/mod.rs
+++ js/src/algorithm/geo/mod.rs
@@ -5,7 +5,7 @@ pub mod chamberlain_duquette_area;
 pub mod convex_hull;
 pub mod dimensions;
 pub mod euclidean_length;
+pub mod geodesic_area;
 pub mod geodesic_length;
 pub mod haversine_length;
 pub mod vincenty_length;
-pub mod geodesic_area;
