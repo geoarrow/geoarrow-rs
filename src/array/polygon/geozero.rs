8a1a95824a9390b607548567692b1d979ac44f9f
Kyle Barron <kylebarron2@gmail.com>
2023-07-31 12:08:01 -0400

    impl polygon

--- src/array/polygon/geozero.rs
+++ src/array/polygon/geozero.rs
@@ -2,7 +2,7 @@ use crate::GeometryArrayTrait;
 use arrow2::types::Offset;
 use geozero::{GeomProcessor, GeozeroGeometry};
 
-use crate::array::PolygonArray;
+use crate::array::{MutablePolygonArray, PolygonArray};
 
 impl<O: Offset> GeozeroGeometry for PolygonArray<O> {
     fn process_geom<P: GeomProcessor>(&self, processor: &mut P) -> geozero::error::Result<()>
@@ -45,11 +45,89 @@ impl<O: Offset> GeozeroGeometry for PolygonArray<O> {
     }
 }
 
+/// GeoZero trait to convert to GeoArrow PolygonArray.
+pub trait ToGeoArrowPolygonArray<O: Offset> {
+    /// Convert to GeoArrow PolygonArray
+    fn to_line_string_array(&self) -> geozero::error::Result<PolygonArray<O>>;
+
+    /// Convert to a GeoArrow MutablePolygonArray
+    fn to_mutable_line_string_array(&self) -> geozero::error::Result<MutablePolygonArray<O>>;
+}
+
+impl<T: GeozeroGeometry, O: Offset> ToGeoArrowPolygonArray<O> for T {
+    fn to_line_string_array(&self) -> geozero::error::Result<PolygonArray<O>> {
+        Ok(self.to_mutable_line_string_array()?.into())
+    }
+
+    fn to_mutable_line_string_array(&self) -> geozero::error::Result<MutablePolygonArray<O>> {
+        let mut mutable_array = MutablePolygonArray::<O>::new();
+        self.process_geom(&mut mutable_array)?;
+        // todo!();
+        Ok(mutable_array)
+    }
+}
+
+#[allow(unused_variables)]
+impl<O: Offset> GeomProcessor for MutablePolygonArray<O> {
+    fn geometrycollection_begin(&mut self, size: usize, idx: usize) -> geozero::error::Result<()> {
+        // reserve `size` geometries
+        self.reserve(0, 0, size);
+        Ok(())
+    }
+
+    fn geometrycollection_end(&mut self, idx: usize) -> geozero::error::Result<()> {
+        // self.shrink_to_fit()
+        Ok(())
+    }
+
+    fn xy(&mut self, x: f64, y: f64, idx: usize) -> geozero::error::Result<()> {
+        // # Safety:
+        // This upholds invariants because we call try_push_length in multipoint_begin to ensure
+        // offset arrays are correct.
+        unsafe { self.push_xy(x, y).unwrap() }
+        Ok(())
+    }
+
+    // Here, size is the number of rings in the polygon
+    fn polygon_begin(
+        &mut self,
+        tagged: bool,
+        size: usize,
+        idx: usize,
+    ) -> geozero::error::Result<()> {
+        // reserve `size` rings
+        self.reserve(0, size, 0);
+
+        // # Safety:
+        // This upholds invariants because we separately update the ring offsets in
+        // linestring_begin
+        unsafe { self.try_push_geom_offset(size).unwrap() }
+        Ok(())
+    }
+
+    fn linestring_begin(
+        &mut self,
+        tagged: bool,
+        size: usize,
+        idx: usize,
+    ) -> geozero::error::Result<()> {
+        // reserve `size` coordinates
+        self.reserve(size, 0, 0);
+
+        // # Safety:
+        // This upholds invariants because we separately update the geometry offsets in
+        // polygon_begin
+        unsafe { self.try_push_ring_offset(size).unwrap() }
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::test::polygon::{p0, p1};
-
     use super::*;
+    use crate::test::polygon::{p0, p1};
+    use geo::Geometry;
+    use geozero::error::Result;
     use geozero::ToWkt;
 
     #[test]
@@ -58,6 +136,20 @@ mod test {
         let wkt = arr.to_wkt()?;
         let expected = "GEOMETRYCOLLECTION(POLYGON((-111 45,-111 41,-104 41,-104 45,-111 45)),POLYGON((-111 45,-111 41,-104 41,-104 45,-111 45),(-110 44,-110 42,-105 42,-105 44,-110 44)))";
         assert_eq!(wkt, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn from_geozero() -> Result<()> {
+        let geo = Geometry::GeometryCollection(
+            vec![p0(), p1()]
+                .into_iter()
+                .map(Geometry::Polygon)
+                .collect(),
+        );
+        let multi_point_array: PolygonArray<i32> = geo.to_line_string_array().unwrap();
+        assert_eq!(multi_point_array.value_as_geo(0), p0());
+        assert_eq!(multi_point_array.value_as_geo(1), p1());
         Ok(())
     }
 }
