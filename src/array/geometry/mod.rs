9cbb8a2a140f7680fbc67245b2496632a5a18143
Kyle Barron <kylebarron2@gmail.com>
2023-08-01 16:54:07 -0400

    Move geometry rstar impl into scalar mod (#125)

--- src/array/geometry/mod.rs
+++ src/array/geometry/mod.rs
@@ -5,4 +5,3 @@ pub use array::GeometryArray;
 mod array;
 mod iterator;
 // mod mutable;
-mod rstar;
--- src/scalar/geometry/scalar.rs
+++ src/scalar/geometry/scalar.rs
@@ -2,6 +2,7 @@ use crate::geo_traits::{GeometryTrait, GeometryType};
 use crate::scalar::*;
 use crate::trait_::GeometryScalarTrait;
 use arrow2::types::Offset;
+use rstar::{RTreeObject, AABB};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Geometry<'a, O: Offset> {
@@ -68,6 +69,38 @@ impl<'a, O: Offset> GeometryTrait<'a> for Geometry<'a, O> {
             // Geometry::GeometryCollection(p) => GeometryType::GeometryCollection(p),
             Geometry::Rect(p) => GeometryType::Rect(p),
             _ => todo!(),
+        }
+    }
+}
+
+impl<O: Offset> RTreeObject for Geometry<'_, O> {
+    type Envelope = AABB<[f64; 2]>;
+
+    fn envelope(&self) -> Self::Envelope {
+        match self {
+            Geometry::Point(geom) => geom.envelope(),
+            Geometry::LineString(geom) => geom.envelope(),
+            Geometry::Polygon(geom) => geom.envelope(),
+            Geometry::MultiPoint(geom) => geom.envelope(),
+            Geometry::MultiLineString(geom) => geom.envelope(),
+            Geometry::MultiPolygon(geom) => geom.envelope(),
+            Geometry::WKB(geom) => geom.envelope(),
+            Geometry::Rect(geom) => geom.envelope(),
+        }
+    }
+}
+
+impl<O: Offset> From<Geometry<'_, O>> for geo::Geometry {
+    fn from(value: Geometry<'_, O>) -> Self {
+        match value {
+            Geometry::Point(geom) => geom.into(),
+            Geometry::LineString(geom) => geom.into(),
+            Geometry::Polygon(geom) => geom.into(),
+            Geometry::MultiPoint(geom) => geom.into(),
+            Geometry::MultiLineString(geom) => geom.into(),
+            Geometry::MultiPolygon(geom) => geom.into(),
+            Geometry::WKB(geom) => geom.into(),
+            Geometry::Rect(geom) => geom.into(),
         }
     }
 }
