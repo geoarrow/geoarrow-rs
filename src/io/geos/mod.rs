b467963285f47a2cf3ba16000fb09e591c1204ed
Kyle Barron <kylebarron2@gmail.com>
2023-08-07 22:01:17 -0400

    Move GEOS io to io/ folder

---
+++ src/io/geos/mod.rs
@@ -0,0 +1,8 @@
+pub mod binary;
+pub mod geometrycollection;
+pub mod linestring;
+pub mod multilinestring;
+pub mod multipoint;
+pub mod multipolygon;
+pub mod point;
+pub mod polygon;
--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,3 +1,5 @@
 #[cfg(feature = "geozero")]
+pub mod geos;
+#[cfg(feature = "geozero")]
 pub mod geozero;
 pub mod native;
