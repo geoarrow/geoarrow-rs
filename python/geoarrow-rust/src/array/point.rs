834d00501a5d32b680595960892e900f493f1c8b
Kyle Barron <kylebarron2@gmail.com>
2023-08-10 17:48:50 -0400

    python capsule

--- python/geoarrow-rust/src/array/point.rs
+++ python/geoarrow-rust/src/array/point.rs
@@ -1,7 +1,19 @@
+use std::ffi::CString;
+
 use crate::ffi::{from_py_array, to_py_array};
+use arrow2::datatypes::Field;
+use arrow2::ffi;
 use geoarrow::GeometryArrayTrait;
 use pyo3::prelude::*;
-use pyo3::types::PyType;
+use pyo3::types::{PyCapsule, PyType};
+
+#[repr(C)]
+struct CDataCapsule {
+    pub schema: *const ffi::ArrowSchema,
+    pub array: *const ffi::ArrowArray,
+}
+
+unsafe impl Send for CDataCapsule {}
 
 #[pyclass]
 pub struct PointArray(pub(crate) geoarrow::array::PointArray);
@@ -11,6 +23,37 @@ impl PointArray {
     #[classmethod]
     fn from_arrow(cls: &PyType, ob: &PyAny) -> PyResult<Self> {
         ob.extract()
+    }
+
+    pub fn from_capsule(ob: &PyAny) -> PyResult<Self> {
+        let cap: PyCapsule = ob.extract()?;
+        cap.
+        let cap = unsafe { PyCapsule::import(py, name)}
+
+    }
+
+    pub fn to_capsule(&self) -> PyResult<PyObject> {
+        let array = self.0.clone().into_arrow();
+
+        Python::with_gil(|py| {
+            let name = CString::new("geoarrow.point").unwrap();
+            let schema = Box::new(ffi::export_field_to_c(&Field::new(
+                "",
+                array.data_type().clone(),
+                true,
+            )));
+            let array = Box::new(ffi::export_array_to_c(array));
+
+            let schema_ptr: *const arrow2::ffi::ArrowSchema = &*schema;
+            let array_ptr: *const arrow2::ffi::ArrowArray = &*array;
+
+            let rust_capsule = CDataCapsule {
+                schema: schema_ptr,
+                array: array_ptr,
+            };
+            let capsule = PyCapsule::new(py, rust_capsule, Some(name)).unwrap();
+            Ok(capsule.into())
+        })
     }
 }
 
