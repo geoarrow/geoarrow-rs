4a87ad51778b865a69691bb3bd57bce93a59b0ab
Kyle Barron <kylebarron2@gmail.com>
2023-08-28 22:20:18 -0400

    move to package.json

--- js/scripts/build_wrapper.sh
+++
@@ -1,4 +0,0 @@
-# Create @geoarrow/geoparquet-wasm
-ENV=$ENV FEATURES="--no-default-features --features debug --features parquet" NAME="@geoarrow/geoparquet-wasm" bash ./scripts/build.sh
-# Create @geoarrow/flatgeobuf-wasm
-ENV=$ENV FEATURES="--no-default-features --features debug --features flatgeobuf" NAME="@geoarrow/flatgeobuf-wasm" bash ./scripts/build.sh
