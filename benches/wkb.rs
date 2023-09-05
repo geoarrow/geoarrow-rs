bdeb3ae67401d3213b41dc28a371a0c8c691f0d9
Kyle Barron <kylebarron2@gmail.com>
2023-09-05 00:29:25 -0400

    benchmark parsing WKB array directly to geo::Geometry using my own parser

--- benches/wkb.rs
+++ benches/wkb.rs
@@ -4,6 +4,8 @@ use arrow2::error::Result;
 use arrow2::io::parquet::read;
 use criterion::{criterion_group, criterion_main, Criterion};
 use geoarrow2::array::{MultiPolygonArray, WKBArray};
+use geoarrow2::geo_traits::{CoordTrait, LineStringTrait, MultiPolygonTrait, PolygonTrait};
+use geoarrow2::scalar::WKB;
 use geoarrow2::trait_::GeometryScalarTrait;
 
 fn load_parquet() -> Result<WKBArray<i32>> {
@@ -31,6 +33,53 @@ fn load_parquet() -> Result<WKBArray<i32>> {
     Ok(wkb_arrays.get(0).unwrap().clone())
 }
 
+fn wkb_linear_ring_to_geo<'a>(
+    wkb_linear_ring: impl LineStringTrait<'a, T = f64>,
+) -> geo::LineString {
+    let mut coords = Vec::with_capacity(wkb_linear_ring.num_coords());
+    for coord_idx in 0..wkb_linear_ring.num_coords() {
+        let wkb_coord = wkb_linear_ring.coord(coord_idx).unwrap();
+        let coord = geo::Coord {
+            x: wkb_coord.x(),
+            y: wkb_coord.y(),
+        };
+        coords.push(coord)
+    }
+    geo::LineString::new(coords)
+}
+
+fn wkb_polygon_to_geo<'a>(wkb_polygon: impl PolygonTrait<'a, T = f64>) -> geo::Polygon {
+    let exterior = wkb_linear_ring_to_geo(wkb_polygon.exterior().unwrap());
+
+    let mut interiors = Vec::with_capacity(wkb_polygon.num_interiors());
+    for interior_idx in 0..wkb_polygon.num_interiors() {
+        let interior = wkb_linear_ring_to_geo(wkb_polygon.interior(interior_idx).unwrap());
+        interiors.push(interior);
+    }
+
+    geo::Polygon::new(exterior, interiors)
+}
+
+fn wkb_multi_polygon_to_geo<'a>(
+    wkb_multi_polygon: impl MultiPolygonTrait<'a, T = f64>,
+) -> geo::Geometry {
+    let mut polygons = Vec::with_capacity(wkb_multi_polygon.num_polygons());
+    for polygon_idx in 0..wkb_multi_polygon.num_polygons() {
+        let polygon = wkb_polygon_to_geo(wkb_multi_polygon.polygon(polygon_idx).unwrap());
+        polygons.push(polygon);
+    }
+    let multi_polygon = geo::MultiPolygon::new(polygons);
+    geo::Geometry::MultiPolygon(multi_polygon)
+}
+
+fn parse_directly_to_geo(value: WKBArray<i32>) -> Vec<geo::Geometry> {
+    let wkb_objects: Vec<WKB<'_, i32>> = value.values_iter().collect();
+    wkb_objects
+        .iter()
+        .map(|wkb| wkb_multi_polygon_to_geo(wkb.to_wkb_object().into_multi_polygon()))
+        .collect()
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
     let array = load_parquet().unwrap();
 
@@ -51,6 +100,11 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             })
         },
     );
+    c.bench_function("parse WKBArray directly to Vec<geo::Geometry>", |b| {
+        b.iter(|| {
+            let _out = parse_directly_to_geo(array.clone());
+        })
+    });
     c.bench_function("parse WKBArray to Vec<geo::Geometry>", |b| {
         b.iter(|| {
             // Note: As of Sept 2023, `to_geo` uses geozero. This could change in the future, in
