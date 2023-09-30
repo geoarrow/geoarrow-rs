8d4d4b478b56a8bad50445e03ef32eafd3724b3c
Kyle Barron <kylebarron2@gmail.com>
2023-09-30 13:53:13 -0400

    Start on geos-based algorithms (#201)
    * Start on geos algorithms
    
    * minimal test

--- src/algorithm/geo/area.rs
+++ src/algorithm/geo/area.rs
@@ -101,3 +101,18 @@ impl<O: Offset> Area for GeometryArray<O> {
         fn unsigned_area(&self) -> PrimitiveArray<f64>;
     }
 }
+
+#[cfg(test)]
+mod test {
+    use arrow2::array::Float64Array;
+
+    use super::*;
+    use crate::test::polygon::polygon_arr;
+
+    #[test]
+    fn tmp() {
+        let arr = polygon_arr();
+        let area = arr.unsigned_area();
+        assert_eq!(area, Float64Array::from_vec(vec![28., 18.]));
+    }
+}
---
+++ src/algorithm/geos/area.rs
@@ -0,0 +1,83 @@
+use crate::algorithm::geo::utils::zeroes;
+use crate::array::{
+    GeometryArray, LineStringArray, MultiLineStringArray, MultiPointArray, MultiPolygonArray,
+    PointArray, PolygonArray, WKBArray,
+};
+use crate::error::Result;
+use crate::GeometryArrayTrait;
+use arrow2::array::{MutablePrimitiveArray, PrimitiveArray};
+use arrow2::types::Offset;
+use geos::Geom;
+
+/// Unsigned planar area of a geometry.
+pub trait Area {
+    fn area(&self) -> Result<PrimitiveArray<f64>>;
+}
+
+// Note: this can't (easily) be parameterized in the macro because PointArray is not generic over O
+impl Area for PointArray {
+    fn area(&self) -> Result<PrimitiveArray<f64>> {
+        Ok(zeroes(self.len(), self.validity()))
+    }
+}
+
+/// Implementation where the result is zero.
+macro_rules! zero_impl {
+    ($type:ty) => {
+        impl<O: Offset> Area for $type {
+            fn area(&self) -> Result<PrimitiveArray<f64>> {
+                Ok(zeroes(self.len(), self.validity()))
+            }
+        }
+    };
+}
+
+zero_impl!(LineStringArray<O>);
+zero_impl!(MultiPointArray<O>);
+zero_impl!(MultiLineStringArray<O>);
+
+macro_rules! iter_geos_impl {
+    ($type:ty) => {
+        impl<O: Offset> Area for $type {
+            fn area(&self) -> Result<PrimitiveArray<f64>> {
+                let mut output_array = MutablePrimitiveArray::<f64>::with_capacity(self.len());
+
+                for maybe_g in self.iter_geos() {
+                    if let Some(g) = maybe_g {
+                        let area = g.area()?;
+                        output_array.push(Some(area));
+                    } else {
+                        output_array.push(None);
+                    }
+                }
+
+                Ok(output_array.into())
+            }
+        }
+    };
+}
+
+iter_geos_impl!(PolygonArray<O>);
+iter_geos_impl!(MultiPolygonArray<O>);
+iter_geos_impl!(WKBArray<O>);
+
+impl<O: Offset> Area for GeometryArray<O> {
+    crate::geometry_array_delegate_impl! {
+        fn area(&self) -> Result<PrimitiveArray<f64>>;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use arrow2::array::Float64Array;
+
+    use super::*;
+    use crate::test::polygon::polygon_arr;
+
+    #[test]
+    fn tmp() {
+        let arr = polygon_arr();
+        let area = arr.area().unwrap();
+        assert_eq!(area, Float64Array::from_vec(vec![28., 18.]));
+    }
+}
