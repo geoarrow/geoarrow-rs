6424aca4bf401681af33bab5a6c054fe33c69358
Kyle Barron <kylebarron2@gmail.com>
2023-12-25 12:01:40 -0800

    inline

--- src/array/cast.rs
+++ src/array/cast.rs
@@ -5,6 +5,7 @@ pub trait AsGeometryArray {
     fn as_point_opt(&self) -> Option<&PointArray>;
 
     /// Downcast this to a [`PointArray`] panicking if not possible
+    #[inline]
     fn as_point(&self) -> &PointArray {
         self.as_point_opt().unwrap()
     }
@@ -13,6 +14,7 @@ pub trait AsGeometryArray {
     fn as_line_string_opt(&self) -> Option<&LineStringArray<i32>>;
 
     /// Downcast this to a [`LineStringArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_line_string(&self) -> &LineStringArray<i32> {
         self.as_line_string_opt().unwrap()
     }
@@ -21,6 +23,7 @@ pub trait AsGeometryArray {
     fn as_large_line_string_opt(&self) -> Option<&LineStringArray<i64>>;
 
     /// Downcast this to a [`LineStringArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_line_string(&self) -> &LineStringArray<i64> {
         self.as_large_line_string_opt().unwrap()
     }
@@ -29,6 +32,7 @@ pub trait AsGeometryArray {
     fn as_polygon_opt(&self) -> Option<&PolygonArray<i32>>;
 
     /// Downcast this to a [`PolygonArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_polygon(&self) -> &PolygonArray<i32> {
         self.as_polygon_opt().unwrap()
     }
@@ -37,6 +41,7 @@ pub trait AsGeometryArray {
     fn as_large_polygon_opt(&self) -> Option<&PolygonArray<i64>>;
 
     /// Downcast this to a [`PolygonArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_polygon(&self) -> &PolygonArray<i64> {
         self.as_large_polygon_opt().unwrap()
     }
@@ -45,6 +50,7 @@ pub trait AsGeometryArray {
     fn as_multi_point_opt(&self) -> Option<&MultiPointArray<i32>>;
 
     /// Downcast this to a [`MultiPointArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_multi_point(&self) -> &MultiPointArray<i32> {
         self.as_multi_point_opt().unwrap()
     }
@@ -53,6 +59,7 @@ pub trait AsGeometryArray {
     fn as_large_multi_point_opt(&self) -> Option<&MultiPointArray<i64>>;
 
     /// Downcast this to a [`MultiPointArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_multi_point(&self) -> &MultiPointArray<i64> {
         self.as_large_multi_point_opt().unwrap()
     }
@@ -62,6 +69,7 @@ pub trait AsGeometryArray {
     fn as_multi_line_string_opt(&self) -> Option<&MultiLineStringArray<i32>>;
 
     /// Downcast this to a [`MultiLineStringArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_multi_line_string(&self) -> &MultiLineStringArray<i32> {
         self.as_multi_line_string_opt().unwrap()
     }
@@ -71,6 +79,7 @@ pub trait AsGeometryArray {
     fn as_large_multi_line_string_opt(&self) -> Option<&MultiLineStringArray<i64>>;
 
     /// Downcast this to a [`MultiLineStringArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_multi_line_string(&self) -> &MultiLineStringArray<i64> {
         self.as_large_multi_line_string_opt().unwrap()
     }
@@ -80,6 +89,7 @@ pub trait AsGeometryArray {
     fn as_multi_polygon_opt(&self) -> Option<&MultiPolygonArray<i32>>;
 
     /// Downcast this to a [`MultiPolygonArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_multi_polygon(&self) -> &MultiPolygonArray<i32> {
         self.as_multi_polygon_opt().unwrap()
     }
@@ -89,6 +99,7 @@ pub trait AsGeometryArray {
     fn as_large_multi_polygon_opt(&self) -> Option<&MultiPolygonArray<i64>>;
 
     /// Downcast this to a [`MultiPolygonArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_multi_polygon(&self) -> &MultiPolygonArray<i64> {
         self.as_large_multi_polygon_opt().unwrap()
     }
@@ -98,6 +109,7 @@ pub trait AsGeometryArray {
     fn as_mixed_opt(&self) -> Option<&MixedGeometryArray<i32>>;
 
     /// Downcast this to a [`MixedGeometryArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_mixed(&self) -> &MixedGeometryArray<i32> {
         self.as_mixed_opt().unwrap()
     }
@@ -107,6 +119,7 @@ pub trait AsGeometryArray {
     fn as_large_mixed_opt(&self) -> Option<&MixedGeometryArray<i64>>;
 
     /// Downcast this to a [`MixedGeometryArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_mixed(&self) -> &MixedGeometryArray<i64> {
         self.as_large_mixed_opt().unwrap()
     }
@@ -116,6 +129,7 @@ pub trait AsGeometryArray {
     fn as_geometry_collection_opt(&self) -> Option<&GeometryCollectionArray<i32>>;
 
     /// Downcast this to a [`GeometryCollectionArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_geometry_collection(&self) -> &GeometryCollectionArray<i32> {
         self.as_geometry_collection_opt().unwrap()
     }
@@ -125,6 +139,7 @@ pub trait AsGeometryArray {
     fn as_large_geometry_collection_opt(&self) -> Option<&GeometryCollectionArray<i64>>;
 
     /// Downcast this to a [`GeometryCollectionArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_geometry_collection(&self) -> &GeometryCollectionArray<i64> {
         self.as_large_geometry_collection_opt().unwrap()
     }
@@ -133,6 +148,7 @@ pub trait AsGeometryArray {
     fn as_wkb_opt(&self) -> Option<&WKBArray<i32>>;
 
     /// Downcast this to a [`WKBArray`] with `i32` offsets panicking if not possible
+    #[inline]
     fn as_wkb(&self) -> &WKBArray<i32> {
         self.as_wkb_opt().unwrap()
     }
@@ -141,6 +157,7 @@ pub trait AsGeometryArray {
     fn as_large_wkb_opt(&self) -> Option<&WKBArray<i64>>;
 
     /// Downcast this to a [`WKBArray`] with `i64` offsets panicking if not possible
+    #[inline]
     fn as_large_wkb(&self) -> &WKBArray<i64> {
         self.as_large_wkb_opt().unwrap()
     }
@@ -149,80 +166,99 @@ pub trait AsGeometryArray {
     fn as_rect_opt(&self) -> Option<&RectArray>;
 
     /// Downcast this to a [`RectArray`] panicking if not possible
+    #[inline]
     fn as_rect(&self) -> &RectArray {
         self.as_rect_opt().unwrap()
     }
 }
 
 impl AsGeometryArray for &dyn GeometryArrayTrait {
+    #[inline]
     fn as_point_opt(&self) -> Option<&PointArray> {
         self.as_any().downcast_ref::<PointArray>()
     }
 
+    #[inline]
     fn as_line_string_opt(&self) -> Option<&LineStringArray<i32>> {
         self.as_any().downcast_ref::<LineStringArray<i32>>()
     }
 
+    #[inline]
     fn as_large_line_string_opt(&self) -> Option<&LineStringArray<i64>> {
         self.as_any().downcast_ref::<LineStringArray<i64>>()
     }
 
+    #[inline]
     fn as_polygon_opt(&self) -> Option<&PolygonArray<i32>> {
         self.as_any().downcast_ref::<PolygonArray<i32>>()
     }
 
+    #[inline]
     fn as_large_polygon_opt(&self) -> Option<&PolygonArray<i64>> {
         self.as_any().downcast_ref::<PolygonArray<i64>>()
     }
 
+    #[inline]
     fn as_multi_point_opt(&self) -> Option<&MultiPointArray<i32>> {
         self.as_any().downcast_ref::<MultiPointArray<i32>>()
     }
 
+    #[inline]
     fn as_large_multi_point_opt(&self) -> Option<&MultiPointArray<i64>> {
         self.as_any().downcast_ref::<MultiPointArray<i64>>()
     }
 
+    #[inline]
     fn as_multi_line_string_opt(&self) -> Option<&MultiLineStringArray<i32>> {
         self.as_any().downcast_ref::<MultiLineStringArray<i32>>()
     }
 
+    #[inline]
     fn as_large_multi_line_string_opt(&self) -> Option<&MultiLineStringArray<i64>> {
         self.as_any().downcast_ref::<MultiLineStringArray<i64>>()
     }
 
+    #[inline]
     fn as_multi_polygon_opt(&self) -> Option<&MultiPolygonArray<i32>> {
         self.as_any().downcast_ref::<MultiPolygonArray<i32>>()
     }
 
+    #[inline]
     fn as_large_multi_polygon_opt(&self) -> Option<&MultiPolygonArray<i64>> {
         self.as_any().downcast_ref::<MultiPolygonArray<i64>>()
     }
 
+    #[inline]
     fn as_mixed_opt(&self) -> Option<&MixedGeometryArray<i32>> {
         self.as_any().downcast_ref::<MixedGeometryArray<i32>>()
     }
 
+    #[inline]
     fn as_large_mixed_opt(&self) -> Option<&MixedGeometryArray<i64>> {
         self.as_any().downcast_ref::<MixedGeometryArray<i64>>()
     }
 
+    #[inline]
     fn as_geometry_collection_opt(&self) -> Option<&GeometryCollectionArray<i32>> {
         self.as_any().downcast_ref::<GeometryCollectionArray<i32>>()
     }
 
+    #[inline]
     fn as_large_geometry_collection_opt(&self) -> Option<&GeometryCollectionArray<i64>> {
         self.as_any().downcast_ref::<GeometryCollectionArray<i64>>()
     }
 
+    #[inline]
     fn as_wkb_opt(&self) -> Option<&WKBArray<i32>> {
         self.as_any().downcast_ref::<WKBArray<i32>>()
     }
 
+    #[inline]
     fn as_large_wkb_opt(&self) -> Option<&WKBArray<i64>> {
         self.as_any().downcast_ref::<WKBArray<i64>>()
     }
 
+    #[inline]
     fn as_rect_opt(&self) -> Option<&RectArray> {
         self.as_any().downcast_ref::<RectArray>()
     }
