0fe2f101c51bb1e497fbfe837378d9f3e5074c68
Kyle Barron <kylebarron2@gmail.com>
2023-12-27 01:09:45 -0500

    python: from and to wkb (#342)

--- python/core/src/io/wkb.rs
+++ python/core/src/io/wkb.rs
@@ -1,8 +1,10 @@
+use geoarrow::array::AsGeometryArray;
 use geoarrow::array::{from_arrow_array, CoordType};
 use geoarrow::datatypes::GeoDataType;
 use geoarrow::io::wkb::{from_wkb as _from_wkb, to_wkb as _to_wkb};
 use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
+use pyo3::types::PyType;
 
 use crate::array::*;
 use crate::ffi::from_python::import_arrow_c_array;
@@ -54,3 +56,76 @@ pub fn to_wkb(ob: &PyAny) -> PyResult<WKBArray> {
     let array = from_arrow_array(&array, &field).unwrap();
     Ok(WKBArray(_to_wkb(array.as_ref())))
 }
+
+macro_rules! impl_from_wkb {
+    ($array:ty, $builder:ty) => {
+        #[pymethods]
+        impl $array {
+            #[classmethod]
+            pub fn from_wkb(_cls: &PyType, ob: &PyAny) -> PyResult<$array> {
+                let (array, field) = import_arrow_c_array(ob)?;
+                let array = from_arrow_array(&array, &field).unwrap();
+                let ref_array = array.as_ref();
+                match array.data_type() {
+                    GeoDataType::WKB => {
+                        let wkb_arr = ref_array.as_wkb();
+                        let builder = <$builder>::from_wkb(
+                            wkb_arr.iter().collect::<Vec<_>>().as_slice(),
+                            Default::default(),
+                        )
+                        .unwrap();
+                        Ok(builder.finish().into())
+                    }
+                    GeoDataType::LargeWKB => {
+                        let wkb_arr = ref_array.as_large_wkb();
+                        let builder = <$builder>::from_wkb(
+                            wkb_arr.iter().collect::<Vec<_>>().as_slice(),
+                            Default::default(),
+                        )
+                        .unwrap();
+                        Ok(builder.finish().into())
+                    }
+                    other => Err(PyTypeError::new_err(format!(
+                        "Unexpected array type {:?}",
+                        other
+                    ))),
+                }
+            }
+        }
+    };
+}
+
+impl_from_wkb!(PointArray, geoarrow::array::PointBuilder);
+impl_from_wkb!(LineStringArray, geoarrow::array::LineStringBuilder<i32>);
+impl_from_wkb!(PolygonArray, geoarrow::array::PolygonBuilder<i32>);
+impl_from_wkb!(MultiPointArray, geoarrow::array::MultiPointBuilder<i32>);
+impl_from_wkb!(
+    MultiLineStringArray,
+    geoarrow::array::MultiLineStringBuilder<i32>
+);
+impl_from_wkb!(MultiPolygonArray, geoarrow::array::MultiPolygonBuilder<i32>);
+
+// TODO: handle extra argument
+// impl_from_wkb!(MixedGeometryArray, geoarrow::array::MixedGeometryBuilder<i32>);
+// impl_from_wkb!(GeometryCollectionArray, geoarrow::array::GeometryCollectionBuilder<i32>);
+
+macro_rules! impl_to_wkb {
+    ($array:ty) => {
+        #[pymethods]
+        impl $array {
+            pub fn to_wkb(&self) -> PyResult<WKBArray> {
+                let wkb_arr = geoarrow::array::WKBArray::<i32>::from(&self.0);
+                Ok(wkb_arr.into())
+            }
+        }
+    };
+}
+
+impl_to_wkb!(PointArray);
+impl_to_wkb!(LineStringArray);
+impl_to_wkb!(PolygonArray);
+impl_to_wkb!(MultiPointArray);
+impl_to_wkb!(MultiLineStringArray);
+impl_to_wkb!(MultiPolygonArray);
+impl_to_wkb!(MixedGeometryArray);
+impl_to_wkb!(GeometryCollectionArray);
