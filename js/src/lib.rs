ab63b2b41666198717a45ebf045b16be47145e73
Kyle Barron <kylebarron2@gmail.com>
2023-12-29 17:51:01 -0700

    fmt

--- js/src/lib.rs
+++ js/src/lib.rs
@@ -2,13 +2,13 @@ pub mod algorithm;
 pub mod broadcasting;
 pub mod data;
 pub mod error;
-pub mod table;
-pub mod vector;
 pub mod ffi;
 pub mod io;
 #[cfg(feature = "geodesy")]
 pub mod reproject;
 pub mod scalar;
+pub mod table;
+pub mod vector;
 // pub mod transform_origin;
 pub mod utils;
 
