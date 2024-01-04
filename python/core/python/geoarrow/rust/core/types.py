9f94d506cbf2ee62bee07435fd97a8e80ef51e67
Kyle Barron <kylebarron2@gmail.com>
2024-01-04 00:26:57 -0500

    Python table FFI (#379)
    amazing!
    
    ```py
    import geopandas as gpd
    import pyarrow as pa
    from geoarrow.rust.core import GeoTable
    from lonboard._geoarrow.geopandas_interop import geopandas_to_geoarrow
    
    gdf = gpd.read_file(gpd.datasets.get_path("naturalearth_cities"))
    py_geotable = geopandas_to_geoarrow(gdf)
    
    rust_table = GeoTable.from_arrow(py_geotable)
    
    pa_table = pa.table(rust_table)
    py_geotable.schema == pa_table.schema
    ```

--- python/core/python/geoarrow/rust/core/types.py
+++ python/core/python/geoarrow/rust/core/types.py
@@ -4,9 +4,16 @@ from typing import Protocol, Tuple
 
 
 class ArrowArrayExportable(Protocol):
-    """An Arrow or GeoArrow array from a local or remote (e.g. geoarrow.c) source."""
+    """An Arrow or GeoArrow array from an Arrow producer (e.g. geoarrow.c)."""
 
     def __arrow_c_array__(
         self, requested_schema: object | None = None
     ) -> Tuple[object, object]:
+        ...
+
+
+class ArrowStreamExportable(Protocol):
+    """An Arrow or GeoArrow ChunkedArray or Table from an Arrow producer (e.g. geoarrow.c)."""
+
+    def __arrow_c_stream__(self, requested_schema: object | None = None) -> object:
         ...
