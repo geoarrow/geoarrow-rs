69e1f64a9a631b3bed2ca854a535fda5b59528cf
Kyle Barron <kylebarron2@gmail.com>
2024-01-08 10:18:51 -0500

    python: bump to 0.1.0

--- python/core/src/algorithm/geo/geodesic_length.rs
+++ python/core/src/algorithm/geo/geodesic_length.rs
@@ -1,4 +1,7 @@
 use crate::array::*;
+use crate::chunked_array::*;
+use crate::error::PyGeoArrowResult;
+use geoarrow::algorithm::geo::GeodesicLength;
 use pyo3::prelude::*;
 
 macro_rules! impl_geodesic_length {
@@ -13,7 +16,6 @@ macro_rules! impl_geodesic_length {
             ///
             /// [Karney (2013)]:  https://arxiv.org/pdf/1109.4448.pdf
             pub fn geodesic_length(&self) -> Float64Array {
-                use geoarrow::algorithm::geo::GeodesicLength;
                 GeodesicLength::geodesic_length(&self.0).into()
             }
         }
@@ -24,3 +26,26 @@ impl_geodesic_length!(PointArray);
 impl_geodesic_length!(MultiPointArray);
 impl_geodesic_length!(LineStringArray);
 impl_geodesic_length!(MultiLineStringArray);
+
+macro_rules! impl_chunked {
+    ($struct_name:ident) => {
+        #[pymethods]
+        impl $struct_name {
+            /// Determine the length of a geometry on an ellipsoidal model of the earth.
+            ///
+            /// This uses the geodesic measurement methods given by [Karney (2013)]. As opposed to
+            /// older methods like Vincenty, this method is accurate to a few nanometers and always
+            /// converges.
+            ///
+            /// [Karney (2013)]:  https://arxiv.org/pdf/1109.4448.pdf
+            pub fn geodesic_length(&self) -> PyGeoArrowResult<ChunkedFloat64Array> {
+                Ok(GeodesicLength::geodesic_length(&self.0)?.into())
+            }
+        }
+    };
+}
+
+impl_chunked!(ChunkedPointArray);
+impl_chunked!(ChunkedMultiPointArray);
+impl_chunked!(ChunkedLineStringArray);
+impl_chunked!(ChunkedMultiLineStringArray);
