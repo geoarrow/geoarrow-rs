3ee2b2b55677f5be84af9f7cd614f048d54eae38
Kyle Barron <kylebarron2@gmail.com>
2024-01-14 01:03:16 -0500

    add file

--- src/io/geos/scalar/mod.rs
+++ src/io/geos/scalar/mod.rs
@@ -1,18 +1,19 @@
-pub mod binary;
-pub mod coord;
-pub mod geometry;
-pub mod geometrycollection;
-pub mod linearring;
-pub mod linestring;
-pub mod multilinestring;
-pub mod multipoint;
-pub mod multipolygon;
-pub mod point;
-pub mod polygon;
+mod binary;
+mod coord;
+mod geometry;
+mod geometrycollection;
+mod linearring;
+mod linestring;
+mod multi_line_string;
+mod multipoint;
+mod multipolygon;
+mod point;
+mod polygon;
 
 pub use linearring::GEOSConstLinearRing;
 pub use linestring::{GEOSConstLineString, GEOSLineString};
-pub use multilinestring::GEOSMultiLineString;
+// Different case is hack around weird github actions bug in #427.
+pub use multi_line_string::GEOSMultiLineString;
 pub use multipoint::GEOSMultiPoint;
 pub use multipolygon::GEOSMultiPolygon;
 pub use point::{GEOSConstPoint, GEOSPoint};
---
+++ src/io/geos/scalar/multi_line_string.rs
@@ -0,0 +1,65 @@
+use crate::error::{GeoArrowError, Result};
+use crate::geo_traits::MultiLineStringTrait;
+use crate::io::geos::scalar::GEOSConstLineString;
+use crate::scalar::MultiLineString;
+use arrow_array::OffsetSizeTrait;
+use geos::{Geom, GeometryTypes};
+
+impl<'b, O: OffsetSizeTrait> TryFrom<MultiLineString<'_, O>> for geos::Geometry<'b> {
+    type Error = GeoArrowError;
+
+    fn try_from(value: MultiLineString<'_, O>) -> Result<geos::Geometry<'b>> {
+        geos::Geometry::try_from(&value)
+    }
+}
+
+impl<'a, 'b, O: OffsetSizeTrait> TryFrom<&'a MultiLineString<'_, O>> for geos::Geometry<'b> {
+    type Error = GeoArrowError;
+
+    fn try_from(value: &'a MultiLineString<'_, O>) -> Result<geos::Geometry<'b>> {
+        let num_lines = value.num_lines();
+        let mut geos_geoms = Vec::with_capacity(num_lines);
+
+        for line_idx in 0..num_lines {
+            let line = value.line(line_idx).unwrap();
+            geos_geoms.push(line.try_into()?);
+        }
+
+        Ok(geos::Geometry::create_multiline_string(geos_geoms)?)
+    }
+}
+/// A GEOS geometry known to be a MultiLineString
+#[derive(Clone)]
+pub struct GEOSMultiLineString<'a>(pub(crate) geos::Geometry<'a>);
+
+impl<'a> GEOSMultiLineString<'a> {
+    pub fn new_unchecked(geom: geos::Geometry<'a>) -> Self {
+        Self(geom)
+    }
+
+    #[allow(dead_code)]
+    pub fn try_new(geom: geos::Geometry<'a>) -> Result<Self> {
+        if matches!(geom.geometry_type(), GeometryTypes::MultiLineString) {
+            Ok(Self(geom))
+        } else {
+            Err(GeoArrowError::General(
+                "Geometry type must be multi line string".to_string(),
+            ))
+        }
+    }
+
+    pub fn num_lines(&self) -> usize {
+        self.0.get_num_geometries().unwrap()
+    }
+
+    #[allow(dead_code)]
+    pub fn line(&'a self, i: usize) -> Option<GEOSConstLineString<'a, '_>> {
+        if i > (self.num_lines()) {
+            return None;
+        }
+
+        Some(GEOSConstLineString::new_unchecked(
+            self.0.get_geometry_n(i).unwrap(),
+        ))
+    }
+}
