319eba83bd607724d9bd6d7d2abab38c36e978ab
Kyle Barron <kylebarron2@gmail.com>
2024-01-15 21:42:17 -0500

    wip: indexed geometry array

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -2,6 +2,7 @@
 
 pub mod broadcasting;
 pub mod geo;
+pub mod geo_index;
 #[cfg(feature = "geodesy")]
 pub mod geodesy;
 #[cfg(feature = "geos")]
---
+++ src/indexed/array.rs
@@ -0,0 +1,31 @@
+use std::sync::Arc;
+
+use crate::algorithm::geo_index::RTree;
+use crate::array::*;
+use crate::GeometryArrayTrait;
+use geo_index::rtree::OwnedRTree;
+
+pub struct IndexedGeometryArray<G: GeometryArrayTrait> {
+    pub(crate) array: G,
+    pub(crate) index: OwnedRTree<f64>,
+}
+
+impl<G: GeometryArrayTrait> IndexedGeometryArray<G> {
+    pub fn new(array: G) -> Self {
+        assert_eq!(array.null_count(), 0);
+        let index = array.as_ref().create_rtree();
+        Self { array, index }
+    }
+}
+
+pub type IndexedPointArray = IndexedGeometryArray<PointArray>;
+pub type IndexedLineStringArray<O> = IndexedGeometryArray<LineStringArray<O>>;
+pub type IndexedPolygonArray<O> = IndexedGeometryArray<PolygonArray<O>>;
+pub type IndexedMultiPointArray<O> = IndexedGeometryArray<MultiPointArray<O>>;
+pub type IndexedMultiLineStringArray<O> = IndexedGeometryArray<MultiLineStringArray<O>>;
+pub type IndexedMultiPolygonArray<O> = IndexedGeometryArray<MultiPolygonArray<O>>;
+pub type IndexedMixedGeometryArray<O> = IndexedGeometryArray<MixedGeometryArray<O>>;
+pub type IndexedGeometryCollectionArray<O> = IndexedGeometryArray<GeometryCollectionArray<O>>;
+pub type IndexedWKBArray<O> = IndexedGeometryArray<WKBArray<O>>;
+pub type IndexedRectArray = IndexedGeometryArray<RectArray>;
+pub type IndexedUnknownGeometryArray = IndexedGeometryArray<Arc<dyn GeometryArrayTrait>>;
