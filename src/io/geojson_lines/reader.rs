3d771b75c71c35f9c1b13b10996a13859aba0474
Kyle Barron <kylebarron2@gmail.com>
2024-01-17 00:59:39 -0500

    Use upstream geojson line reader

--- src/io/geojson_lines/reader.rs
+++ src/io/geojson_lines/reader.rs
@@ -1,6 +1,6 @@
-use geozero::geojson::GeoJsonReader;
+use geozero::geojson::GeoJsonLineReader;
 use geozero::GeozeroDatasource;
-use std::io::{BufRead, Cursor};
+use std::io::BufRead;
 
 use crate::array::CoordType;
 use crate::error::Result;
@@ -13,6 +13,8 @@ use crate::table::GeoTable;
 /// This expects a GeoJSON Feature on each line of a text file, with a newline character separating
 /// each Feature.
 pub fn read_geojson_lines<R: BufRead>(reader: R, batch_size: Option<usize>) -> Result<GeoTable> {
+    let mut geojson_line_reader = GeoJsonLineReader::new(reader);
+
     // TODO: set crs to epsg:4326?
     let options = GeoTableBuilderOptions::new(
         CoordType::Interleaved,
@@ -24,11 +26,6 @@ pub fn read_geojson_lines<R: BufRead>(reader: R, batch_size: Option<usize>) -> R
     );
     let mut geo_table =
         GeoTableBuilder::<MixedGeometryStreamBuilder<i32>>::new_with_options(options);
-
-    for line in reader.lines() {
-        let mut geojson = GeoJsonReader(Cursor::new(line?));
-        geojson.process(&mut geo_table)?;
-    }
-
+    geojson_line_reader.process(&mut geo_table)?;
     geo_table.finish()
 }
