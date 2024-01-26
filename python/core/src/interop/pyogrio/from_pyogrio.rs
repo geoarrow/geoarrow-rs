bde0595a4d3a3186f77b4ac8b2966de2a86b51f4
Kyle Barron <kylebarron2@gmail.com>
2024-01-26 00:15:05 -0500

    Add docstring

--- python/core/src/interop/pyogrio/from_pyogrio.rs
+++ python/core/src/interop/pyogrio/from_pyogrio.rs
@@ -6,9 +6,94 @@ use pyo3::prelude::*;
 use pyo3::types::PyDict;
 use pyo3::PyAny;
 
+/// Read from an OGR data source to a GeoTable
+///
+/// **At the moment, this will always read only 2D geometries.**
+///
+/// Args:
+///     path_or_buffer: A dataset path or URI, or raw buffer.
+///     layer: If an integer is provided, it corresponds to the index of the layer
+///         with the data source. If a string is provided, it must match the name
+///         of the layer in the data source. Defaults to first layer in data source.
+///     encoding: If present, will be used as the encoding for reading string values from
+///         the data source, unless encoding can be inferred directly from the data
+///         source.
+///     columns: List of column names to import from the data source. Column names must
+///         exactly match the names in the data source, and will be returned in
+///         the order they occur in the data source. To avoid reading any columns,
+///         pass an empty list-like.
+///     read_geometry: If True, will read geometry into a GeoSeries. If False, a Pandas DataFrame
+///         will be returned instead. Default: `True`.
+///     skip_features: Number of features to skip from the beginning of the file before
+///         returning features. If greater than available number of features, an
+///         empty DataFrame will be returned. Using this parameter may incur
+///         significant overhead if the driver does not support the capability to
+///         randomly seek to a specific feature, because it will need to iterate
+///         over all prior features.
+///     max_features: Number of features to read from the file. Default: `None`.
+///     where: Where clause to filter features in layer by attribute values. If the data source
+///         natively supports SQL, its specific SQL dialect should be used (eg. SQLite and
+///         GeoPackage: [`SQLITE`][SQLITE], PostgreSQL). If it doesn't, the [`OGRSQL
+///         WHERE`][`OGRSQL_WHERE`] syntax should be used. Note that it is not possible to overrule
+///         the SQL dialect, this is only possible when you use the `sql` parameter.
+///
+///         Examples: `"ISO_A3 = 'CAN'"`, `"POP_EST > 10000000 AND POP_EST < 100000000"`
+///     bbox: If present, will be used to filter records whose geometry intersects this
+///         box. This must be in the same CRS as the dataset. If GEOS is present
+///         and used by GDAL, only geometries that intersect this bbox will be
+///         returned; if GEOS is not available or not used by GDAL, all geometries
+///         with bounding boxes that intersect this bbox will be returned.
+///         Cannot be combined with `mask` keyword.
+///     mask: Shapely geometry, optional (default: None)
+///         If present, will be used to filter records whose geometry intersects
+///         this geometry. This must be in the same CRS as the dataset. If GEOS is
+///         present and used by GDAL, only geometries that intersect this geometry
+///         will be returned; if GEOS is not available or not used by GDAL, all
+///         geometries with bounding boxes that intersect the bounding box of this
+///         geometry will be returned. Requires Shapely >= 2.0.
+///         Cannot be combined with `bbox` keyword.
+///     fids : array-like, optional (default: None)
+///         Array of integer feature id (FID) values to select. Cannot be combined
+///         with other keywords to select a subset (`skip_features`,
+///         `max_features`, `where`, `bbox`, `mask`, or `sql`). Note that
+///         the starting index is driver and file specific (e.g. typically 0 for
+///         Shapefile and 1 for GeoPackage, but can still depend on the specific
+///         file). The performance of reading a large number of features usings FIDs
+///         is also driver specific.
+///     sql: The SQL statement to execute. Look at the sql_dialect parameter for more
+///         information on the syntax to use for the query. When combined with other
+///         keywords like `columns`, `skip_features`, `max_features`,
+///         `where`, `bbox`, or `mask`, those are applied after the SQL query.
+///         Be aware that this can have an impact on performance, (e.g. filtering
+///         with the `bbox` or `mask` keywords may not use spatial indexes).
+///         Cannot be combined with the `layer` or `fids` keywords.
+///     sql_dialect : str, optional (default: None)
+///         The SQL dialect the SQL statement is written in. Possible values:
+///
+///           - **None**: if the data source natively supports SQL, its specific SQL dialect
+///             will be used by default (eg. SQLite and Geopackage: `SQLITE`_, PostgreSQL).
+///             If the data source doesn't natively support SQL, the `OGRSQL`_ dialect is
+///             the default.
+///           - [`'OGRSQL'`][OGRSQL]: can be used on any data source. Performance can suffer
+///             when used on data sources with native support for SQL.
+///           - [`'SQLITE'`][SQLITE]: can be used on any data source. All [spatialite][spatialite]
+///             functions can be used. Performance can suffer on data sources with
+///             native support for SQL, except for Geopackage and SQLite as this is
+///             their native SQL dialect.
+///     **kwargs
+///         Additional driver-specific dataset open options passed to OGR. Invalid
+///         options will trigger a warning.
+///
+/// Returns:
+///     Table
+///
+/// [OGRSQL]: https://gdal.org/user/ogr_sql_dialect.html#ogr-sql-dialect
+/// [OGRSQL_WHERE]: https://gdal.org/user/ogr_sql_dialect.html#where
+/// [SQLITE]: https://gdal.org/user/sql_sqlite_dialect.html#sql-sqlite-dialect
+/// [spatialite]: https://www.gaia-gis.it/gaia-sins/spatialite-sql-latest.html
 #[allow(clippy::too_many_arguments)]
 #[pyfunction]
-#[pyo3(signature = (path_or_buffer, /, layer=None, encoding=None, columns=None, read_geometry=true, force_2d=false, skip_features=0, max_features=None, r#where=None, bbox=None, mask=None, fids=None, sql=None, sql_dialect=None, return_fids=false, batch_size=65536, **kwargs))]
+#[pyo3(signature = (path_or_buffer, /, layer=None, encoding=None, columns=None, read_geometry=true, skip_features=0, max_features=None, r#where=None, bbox=None, mask=None, fids=None, sql=None, sql_dialect=None, return_fids=false, batch_size=65536, **kwargs))]
 pub fn read_pyogrio(
     py: Python,
     path_or_buffer: &PyAny,
@@ -16,7 +101,6 @@ pub fn read_pyogrio(
     encoding: Option<&PyAny>,
     columns: Option<&PyAny>,
     read_geometry: bool,
-    force_2d: bool,
     skip_features: usize,
     max_features: Option<&PyAny>,
     r#where: Option<&PyAny>,
@@ -40,7 +124,8 @@ pub fn read_pyogrio(
     our_kwargs.set_item("encoding", encoding)?;
     our_kwargs.set_item("columns", columns)?;
     our_kwargs.set_item("read_geometry", read_geometry)?;
-    our_kwargs.set_item("force_2d", force_2d)?;
+    // NOTE: We always read only 2D data for now.
+    our_kwargs.set_item("force_2d", true)?;
     our_kwargs.set_item("skip_features", skip_features)?;
     our_kwargs.set_item("max_features", max_features)?;
     our_kwargs.set_item("where", r#where)?;
