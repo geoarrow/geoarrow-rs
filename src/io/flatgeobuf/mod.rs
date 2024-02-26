eb0f4de5813dafb7ffc86b3f4e501bc6675ec423
Kyle Barron <kylebarron2@gmail.com>
2024-02-25 21:56:56 -0500

    fix conditional compilation

--- src/io/flatgeobuf/mod.rs
+++ src/io/flatgeobuf/mod.rs
@@ -3,5 +3,7 @@
 mod reader;
 mod writer;
 
-pub use reader::{read_flatgeobuf, read_flatgeobuf_async};
+pub use reader::read_flatgeobuf;
+#[cfg(feature = "flatgeobuf_async")]
+pub use reader::read_flatgeobuf_async;
 pub use writer::{write_flatgeobuf, write_flatgeobuf_with_options};
--- src/io/flatgeobuf/reader/mod.rs
+++ src/io/flatgeobuf/reader/mod.rs
@@ -1,7 +1,10 @@
+#[cfg(feature = "flatgeobuf_async")]
 mod r#async;
 mod common;
+#[cfg(feature = "flatgeobuf_async")]
 mod object_store_reader;
 mod sync;
 
+#[cfg(feature = "flatgeobuf_async")]
 pub use r#async::read_flatgeobuf_async;
 pub use sync::read_flatgeobuf;
