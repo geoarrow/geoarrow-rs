f62ba29b9757d33afda2241821d20ab57a4ddfd2
Kyle Barron <kyle@developmentseed.org>
2024-03-27 11:31:23 -0400

    Async GeoParquet writer

--- src/io/parquet/writer/async.rs
+++ src/io/parquet/writer/async.rs
@@ -1,5 +1,3 @@
-use std::io::Write;
-
 use crate::error::Result;
 use crate::io::parquet::writer::encode::encode_record_batch;
 use crate::io::parquet::writer::metadata::GeoParquetMetadataBuilder;
@@ -7,34 +5,35 @@ use crate::io::parquet::writer::options::GeoParquetWriterOptions;
 use crate::table::GeoTable;
 use arrow_array::RecordBatch;
 use arrow_schema::Schema;
-use parquet::arrow::ArrowWriter;
+use parquet::arrow::AsyncArrowWriter;
 use parquet::file::metadata::KeyValue;
+use tokio::io::AsyncWrite;
 
-pub fn write_geoparquet<W: Write + Send>(
+pub async fn write_geoparquet_async<W: AsyncWrite + Unpin + Send>(
     table: &mut GeoTable,
     writer: W,
     options: &GeoParquetWriterOptions,
 ) -> Result<()> {
-    let mut parquet_writer = GeoParquetWriter::try_new(writer, table.schema(), options)?;
+    let mut parquet_writer = GeoParquetWriterAsync::try_new(writer, table.schema(), options)?;
 
     for batch in table.batches() {
-        parquet_writer.write_batch(batch)?;
+        parquet_writer.write_batch(batch).await?;
     }
 
-    parquet_writer.finish()?;
+    parquet_writer.finish().await?;
     Ok(())
 }
 
-pub struct GeoParquetWriter<W: Write + Send> {
-    writer: ArrowWriter<W>,
+pub struct GeoParquetWriterAsync<W: AsyncWrite + Unpin + Send> {
+    writer: AsyncArrowWriter<W>,
     metadata_builder: GeoParquetMetadataBuilder,
 }
 
-impl<W: Write + Send> GeoParquetWriter<W> {
+impl<W: AsyncWrite + Unpin + Send> GeoParquetWriterAsync<W> {
     pub fn try_new(writer: W, schema: &Schema, options: &GeoParquetWriterOptions) -> Result<Self> {
         let metadata_builder = GeoParquetMetadataBuilder::try_new(schema, options)?;
 
-        let writer = ArrowWriter::try_new(
+        let writer = AsyncArrowWriter::try_new(
             writer,
             metadata_builder.output_schema.clone(),
             options.writer_properties.clone(),
@@ -46,23 +45,23 @@ impl<W: Write + Send> GeoParquetWriter<W> {
         })
     }
 
-    pub fn write_batch(&mut self, batch: &RecordBatch) -> Result<()> {
+    pub async fn write_batch(&mut self, batch: &RecordBatch) -> Result<()> {
         let encoded_batch = encode_record_batch(batch, &mut self.metadata_builder)?;
-        self.writer.write(&encoded_batch)?;
+        self.writer.write(&encoded_batch).await?;
         Ok(())
     }
 
-    pub fn writer(&self) -> &ArrowWriter<W> {
+    pub fn writer(&self) -> &AsyncArrowWriter<W> {
         &self.writer
     }
 
-    pub fn finish(mut self) -> Result<()> {
+    pub async fn finish(mut self) -> Result<()> {
         if let Some(geo_meta) = self.metadata_builder.finish() {
             let kv_metadata = KeyValue::new("geo".to_string(), serde_json::to_string(&geo_meta)?);
             self.writer.append_key_value_metadata(kv_metadata);
         }
 
-        self.writer.close()?;
+        self.writer.close().await?;
         Ok(())
     }
 }
--- src/io/parquet/writer/mod.rs
+++ src/io/parquet/writer/mod.rs
@@ -1,7 +1,10 @@
+mod r#async;
 mod encode;
 mod metadata;
 mod options;
 mod sync;
 
 pub use options::{GeoParquetWriterEncoding, GeoParquetWriterOptions};
+#[cfg(feature = "parquet_async")]
+pub use r#async::{write_geoparquet_async, GeoParquetWriterAsync};
 pub use sync::{write_geoparquet, GeoParquetWriter};
