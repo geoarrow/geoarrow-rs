26f9a9f3cad457f6629f8eba57f3eeb61518981c
Kyle Barron <kylebarron2@gmail.com>
2024-07-01 23:09:32 -0400

    refactor to_shapely

--- python/core/src/interop/shapely/to_shapely.rs
+++ python/core/src/interop/shapely/to_shapely.rs
@@ -51,238 +51,127 @@ fn coords_to_numpy(py: Python, coords: CoordBuffer) -> PyGeoArrowResult<PyObject
 #[pyfunction]
 pub fn to_shapely(py: Python, input: &Bound<PyAny>) -> PyGeoArrowResult<PyObject> {
     let (array, field) = import_arrow_c_array(input)?;
-    let array = from_arrow_array(&array, &field)?;
-    match array.data_type() {
-        GeoDataType::Point(_) => Ok(PointArray(array.as_ref().as_point().clone())
-            .to_shapely(py)?
-            .to_object(py)),
-        GeoDataType::LineString(_) => Ok(LineStringArray(array.as_ref().as_line_string().clone())
-            .to_shapely(py)?
-            .to_object(py)),
-        GeoDataType::Polygon(_) => Ok(PolygonArray(array.as_ref().as_polygon().clone())
-            .to_shapely(py)?
-            .to_object(py)),
-        GeoDataType::MultiPoint(_) => Ok(MultiPointArray(array.as_ref().as_multi_point().clone())
-            .to_shapely(py)?
-            .to_object(py)),
-        GeoDataType::MultiLineString(_) => Ok(MultiLineStringArray(
-            array.as_ref().as_multi_line_string().clone(),
-        )
-        .to_shapely(py)?
-        .to_object(py)),
+    check_nulls(array.nulls())?;
+    let geo_array = from_arrow_array(&array, &field)?;
+
+    let shapely_mod = import_shapely(py)?;
+    let shapely_geom_type_enum = shapely_mod.getattr(intern!(py, "GeometryType"))?;
+
+    match geo_array.data_type() {
+        GeoDataType::Point(_) => {
+            let geo_array = geo_array.as_ref().as_point().clone();
+            let coords = coords_to_numpy(py, geo_array.coords().clone())?;
+            let args = (
+                shapely_geom_type_enum.getattr(intern!(py, "POINT"))?,
+                coords,
+            );
+            Ok(shapely_mod
+                .call_method1(intern!(py, "from_ragged_array"), args)?
+                .to_object(py))
+        }
+        GeoDataType::LineString(_) => {
+            let geo_array = geo_array.as_ref().as_line_string().clone();
+
+            let coords = coords_to_numpy(py, geo_array.coords().clone())?;
+            let offsets = (geo_array.geom_offsets().to_pyarray_bound(py),);
+
+            let args = (
+                shapely_geom_type_enum.getattr(intern!(py, "LINESTRING"))?,
+                coords,
+                offsets,
+            );
+            Ok(shapely_mod
+                .call_method1(intern!(py, "from_ragged_array"), args)?
+                .to_object(py))
+        }
+        GeoDataType::Polygon(_) => {
+            let geo_array = geo_array.as_ref().as_polygon().clone();
+            let coords = coords_to_numpy(py, geo_array.coords().clone())?;
+            let offsets = (
+                geo_array.ring_offsets().to_pyarray_bound(py),
+                geo_array.geom_offsets().to_pyarray_bound(py),
+            );
+
+            let args = (
+                shapely_geom_type_enum.getattr(intern!(py, "POLYGON"))?,
+                coords,
+                offsets,
+            );
+            Ok(shapely_mod
+                .call_method1(intern!(py, "from_ragged_array"), args)?
+                .to_object(py))
+        }
+        GeoDataType::MultiPoint(_) => {
+            let geo_array = geo_array.as_ref().as_multi_point().clone();
+
+            let coords = coords_to_numpy(py, geo_array.coords().clone())?;
+            let offsets = (geo_array.geom_offsets().to_pyarray_bound(py),);
+
+            let args = (
+                shapely_geom_type_enum.getattr(intern!(py, "MULTIPOINT"))?,
+                coords,
+                offsets,
+            );
+            Ok(shapely_mod
+                .call_method1(intern!(py, "from_ragged_array"), args)?
+                .to_object(py))
+        }
+        GeoDataType::MultiLineString(_) => {
+            let geo_array = geo_array.as_ref().as_multi_line_string().clone();
+
+            let coords = coords_to_numpy(py, geo_array.coords().clone())?;
+            let offsets = (
+                geo_array.ring_offsets().to_pyarray_bound(py),
+                geo_array.geom_offsets().to_pyarray_bound(py),
+            );
+
+            let args = (
+                shapely_geom_type_enum.getattr(intern!(py, "MULTILINESTRING"))?,
+                coords,
+                offsets,
+            );
+            Ok(shapely_mod
+                .call_method1(intern!(py, "from_ragged_array"), args)?
+                .to_object(py))
+        }
         GeoDataType::MultiPolygon(_) => {
-            Ok(MultiPolygonArray(array.as_ref().as_multi_polygon().clone())
-                .to_shapely(py)?
+            let geo_array = geo_array.as_ref().as_multi_polygon().clone();
+            let coords = coords_to_numpy(py, geo_array.coords().clone())?;
+            let offsets = (
+                geo_array.ring_offsets().to_pyarray_bound(py),
+                geo_array.polygon_offsets().to_pyarray_bound(py),
+                geo_array.geom_offsets().to_pyarray_bound(py),
+            );
+
+            let args = (
+                shapely_geom_type_enum.getattr(intern!(py, "MULTIPOLYGON"))?,
+                coords,
+                offsets,
+            );
+            Ok(shapely_mod
+                .call_method1(intern!(py, "from_ragged_array"), args)?
                 .to_object(py))
         }
         GeoDataType::Mixed(_) => {
-            MixedGeometryArray(array.as_ref().as_mixed().clone()).to_shapely(py)
+            let geo_arr = geo_array.as_ref().as_mixed().clone();
+            wkb_array_to_shapely(py, WKBArray(to_wkb(geo_arr.as_ref())))
         }
         GeoDataType::GeometryCollection(_) => {
-            GeometryCollectionArray(array.as_ref().as_geometry_collection().clone()).to_shapely(py)
+            let geo_arr = geo_array.as_ref().as_geometry_collection().clone();
+            wkb_array_to_shapely(py, WKBArray(to_wkb(geo_arr.as_ref())))
         }
-        GeoDataType::WKB => WKBArray(array.as_ref().as_wkb().clone()).to_shapely(py),
+        GeoDataType::WKB => {
+            let arr = WKBArray(geo_array.as_ref().as_wkb().clone());
+            wkb_array_to_shapely(py, arr)
+        }
         t => Err(PyValueError::new_err(format!("unexpected type {:?}", t)).into()),
     }
 }
 
-#[pymethods]
-impl PointArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<Bound<PyAny>> {
-        check_nulls(self.0.nulls())?;
-
-        let shapely_mod = import_shapely(py)?;
-        let shapely_geom_type_enum = shapely_mod.getattr(intern!(py, "GeometryType"))?;
-
-        let coords = coords_to_numpy(py, self.0.coords().clone())?;
-        let args = (
-            shapely_geom_type_enum.getattr(intern!(py, "POINT"))?,
-            coords,
-        );
-        Ok(shapely_mod.call_method1(intern!(py, "from_ragged_array"), args)?)
-    }
-}
-
-#[pymethods]
-impl LineStringArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<Bound<PyAny>> {
-        check_nulls(self.0.nulls())?;
-
-        let shapely_mod = import_shapely(py)?;
-        let shapely_geom_type_enum = shapely_mod.getattr(intern!(py, "GeometryType"))?;
-
-        let coords = coords_to_numpy(py, self.0.coords().clone())?;
-        let offsets = (self.0.geom_offsets().to_pyarray_bound(py),);
-
-        let args = (
-            shapely_geom_type_enum.getattr(intern!(py, "LINESTRING"))?,
-            coords,
-            offsets,
-        );
-        Ok(shapely_mod.call_method1(intern!(py, "from_ragged_array"), args)?)
-    }
-}
-
-#[pymethods]
-impl PolygonArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<Bound<PyAny>> {
-        check_nulls(self.0.nulls())?;
-
-        let shapely_mod = import_shapely(py)?;
-        let shapely_geom_type_enum = shapely_mod.getattr(intern!(py, "GeometryType"))?;
-
-        let coords = coords_to_numpy(py, self.0.coords().clone())?;
-        let offsets = (
-            self.0.ring_offsets().to_pyarray_bound(py),
-            self.0.geom_offsets().to_pyarray_bound(py),
-        );
-
-        let args = (
-            shapely_geom_type_enum.getattr(intern!(py, "POLYGON"))?,
-            coords,
-            offsets,
-        );
-        Ok(shapely_mod.call_method1(intern!(py, "from_ragged_array"), args)?)
-    }
-}
-
-#[pymethods]
-impl MultiPointArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<Bound<PyAny>> {
-        check_nulls(self.0.nulls())?;
-
-        let shapely_mod = import_shapely(py)?;
-        let shapely_geom_type_enum = shapely_mod.getattr(intern!(py, "GeometryType"))?;
-
-        let coords = coords_to_numpy(py, self.0.coords().clone())?;
-        let offsets = (self.0.geom_offsets().to_pyarray_bound(py),);
-
-        let args = (
-            shapely_geom_type_enum.getattr(intern!(py, "MULTIPOINT"))?,
-            coords,
-            offsets,
-        );
-        Ok(shapely_mod.call_method1(intern!(py, "from_ragged_array"), args)?)
-    }
-}
-
-#[pymethods]
-impl MultiLineStringArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<Bound<PyAny>> {
-        check_nulls(self.0.nulls())?;
-
-        let shapely_mod = import_shapely(py)?;
-        let shapely_geom_type_enum = shapely_mod.getattr(intern!(py, "GeometryType"))?;
-
-        let coords = coords_to_numpy(py, self.0.coords().clone())?;
-        let offsets = (
-            self.0.ring_offsets().to_pyarray_bound(py),
-            self.0.geom_offsets().to_pyarray_bound(py),
-        );
-
-        let args = (
-            shapely_geom_type_enum.getattr(intern!(py, "MULTILINESTRING"))?,
-            coords,
-            offsets,
-        );
-        Ok(shapely_mod.call_method1(intern!(py, "from_ragged_array"), args)?)
-    }
-}
-
-#[pymethods]
-impl MultiPolygonArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<Bound<PyAny>> {
-        check_nulls(self.0.nulls())?;
-
-        let shapely_mod = import_shapely(py)?;
-        let shapely_geom_type_enum = shapely_mod.getattr(intern!(py, "GeometryType"))?;
-
-        let coords = coords_to_numpy(py, self.0.coords().clone())?;
-        let offsets = (
-            self.0.ring_offsets().to_pyarray_bound(py),
-            self.0.polygon_offsets().to_pyarray_bound(py),
-            self.0.geom_offsets().to_pyarray_bound(py),
-        );
-
-        let args = (
-            shapely_geom_type_enum.getattr(intern!(py, "MULTIPOLYGON"))?,
-            coords,
-            offsets,
-        );
-        Ok(shapely_mod.call_method1(intern!(py, "from_ragged_array"), args)?)
-    }
-}
-
-#[pymethods]
-impl MixedGeometryArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<PyObject> {
-        check_nulls(self.0.nulls())?;
-        WKBArray(to_wkb(self.0.as_ref())).to_shapely(py)
-    }
-}
-
-#[pymethods]
-impl GeometryCollectionArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<PyObject> {
-        check_nulls(self.0.nulls())?;
-        WKBArray(to_wkb(self.0.as_ref())).to_shapely(py)
-    }
-}
-
-#[pymethods]
-impl WKBArray {
-    /// Convert this array to a shapely array
-    ///
-    /// Returns:
-    ///
-    ///     A shapely array.
-    fn to_shapely<'a>(&'a self, py: Python<'a>) -> PyGeoArrowResult<PyObject> {
-        check_nulls(self.0.nulls())?;
-        let shapely_mod = import_shapely(py)?;
-        let shapely_arr =
-            shapely_mod.call_method1(intern!(py, "from_wkb"), (self.__array__(py)?,))?;
-        Ok(shapely_arr.to_object(py))
-    }
+fn wkb_array_to_shapely(py: Python, arr: WKBArray) -> PyGeoArrowResult<PyObject> {
+    let shapely_mod = import_shapely(py)?;
+    let shapely_arr = shapely_mod.call_method1(intern!(py, "from_wkb"), (arr.__array__(py)?,))?;
+    Ok(shapely_arr.to_object(py))
 }
 
 macro_rules! impl_chunked_to_shapely {
