573dd0a06f2e880360114d6f69e0186ed868aba4
Kyle Barron <kyle@developmentseed.org>
2024-08-26 16:48:42 -0400

    Python: Fix array indexing with negative integers

--- python/core/src/array/getitem.rs
+++ python/core/src/array/getitem.rs
@@ -3,6 +3,7 @@ use crate::ffi::to_python::array::geometry_to_pyobject;
 use crate::scalar::*;
 use geoarrow::trait_::GeometryArrayAccessor;
 use geoarrow::GeometryArrayTrait;
+use pyo3::exceptions::PyIndexError;
 use pyo3::prelude::*;
 
 macro_rules! impl_getitem {
@@ -10,14 +11,21 @@ macro_rules! impl_getitem {
         #[pymethods]
         impl $struct_name {
             /// Access the item at a given index
-            pub fn __getitem__(&self, key: isize) -> Option<$return_type> {
+            pub fn __getitem__(&self, i: isize) -> PyGeoArrowResult<Option<$return_type>> {
                 // Handle negative indexes from the end
-                let index = if key < 0 {
-                    self.0.len() + key as usize
+                let i = if i < 0 {
+                    let i = self.0.len() as isize + i;
+                    if i < 0 {
+                        return Err(PyIndexError::new_err("Index out of range").into());
+                    }
+                    i as usize
                 } else {
-                    key as usize
+                    i as usize
                 };
-                self.0.get(index).map(|geom| $return_type(geom.into()))
+                if i >= self.0.len() {
+                    return Err(PyIndexError::new_err("Index out of range").into());
+                }
+                Ok(self.0.get(i).map(|geom| $return_type(geom.into())))
             }
         }
     };
@@ -36,14 +44,23 @@ impl_getitem!(RectArray, Rect);
 #[pymethods]
 impl MixedGeometryArray {
     /// Access the item at a given index
-    pub fn __getitem__(&self, key: isize) -> Option<PyObject> {
+    pub fn __getitem__(&self, i: isize) -> PyGeoArrowResult<Option<PyObject>> {
         // Handle negative indexes from the end
-        let index = if key < 0 {
-            self.0.len() + key as usize
+        let i = if i < 0 {
+            let i = self.0.len() as isize + i;
+            if i < 0 {
+                return Err(PyIndexError::new_err("Index out of range").into());
+            }
+            i as usize
         } else {
-            key as usize
+            i as usize
         };
-        let geom = self.0.get(index);
-        Python::with_gil(|py| geom.map(|g| geometry_to_pyobject(py, g)))
+        if i >= self.0.len() {
+            return Err(PyIndexError::new_err("Index out of range").into());
+        }
+        let geom = self.0.get(i);
+        Ok(Python::with_gil(|py| {
+            geom.map(|g| geometry_to_pyobject(py, g))
+        }))
     }
 }
--- python/core/src/chunked_array/getitem.rs
+++ python/core/src/chunked_array/getitem.rs
@@ -1,6 +1,8 @@
 use crate::chunked_array::*;
+use crate::error::PyGeoArrowResult;
 use crate::ffi::to_python::array::geometry_to_pyobject;
 use crate::scalar::*;
+use pyo3::exceptions::PyIndexError;
 use pyo3::prelude::*;
 
 macro_rules! impl_getitem {
@@ -8,14 +10,21 @@ macro_rules! impl_getitem {
         #[pymethods]
         impl $struct_name {
             /// Access the item at a given index
-            pub fn __getitem__(&self, key: isize) -> Option<$return_type> {
+            pub fn __getitem__(&self, i: isize) -> PyGeoArrowResult<Option<$return_type>> {
                 // Handle negative indexes from the end
-                let index = if key < 0 {
-                    self.0.len() + key as usize
+                let i = if i < 0 {
+                    let i = self.0.len() as isize + i;
+                    if i < 0 {
+                        return Err(PyIndexError::new_err("Index out of range").into());
+                    }
+                    i as usize
                 } else {
-                    key as usize
+                    i as usize
                 };
-                self.0.get(index).map(|geom| $return_type(geom.into()))
+                if i >= self.0.len() {
+                    return Err(PyIndexError::new_err("Index out of range").into());
+                }
+                Ok(self.0.get(i).map(|geom| $return_type(geom.into())))
             }
         }
     };
@@ -34,14 +43,23 @@ impl_getitem!(ChunkedRectArray, Rect);
 #[pymethods]
 impl ChunkedMixedGeometryArray {
     /// Access the item at a given index
-    pub fn __getitem__(&self, key: isize) -> Option<PyObject> {
+    pub fn __getitem__(&self, i: isize) -> PyGeoArrowResult<Option<PyObject>> {
         // Handle negative indexes from the end
-        let index = if key < 0 {
-            self.0.len() + key as usize
+        let i = if i < 0 {
+            let i = self.0.len() as isize + i;
+            if i < 0 {
+                return Err(PyIndexError::new_err("Index out of range").into());
+            }
+            i as usize
         } else {
-            key as usize
+            i as usize
         };
-        let geom = self.0.get(index);
-        Python::with_gil(|py| geom.map(|g| geometry_to_pyobject(py, g)))
+        if i >= self.0.len() {
+            return Err(PyIndexError::new_err("Index out of range").into());
+        }
+        let geom = self.0.get(i);
+        Ok(Python::with_gil(|py| {
+            geom.map(|g| geometry_to_pyobject(py, g))
+        }))
     }
 }
