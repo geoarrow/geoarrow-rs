bd7704015552e433fb6244e898c814eb2e469a03
Kyle Barron <kylebarron2@gmail.com>
2024-08-26 23:25:36 -0400

    import enum

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -49,76 +49,75 @@ use crate::GeometryArrayTrait;
 /// Convert an Arrow [Array] to a geoarrow GeometryArray
 pub fn from_arrow_array(array: &dyn Array, field: &Field) -> Result<Arc<dyn GeometryArrayTrait>> {
     let data_type = GeoDataType::try_from(field)?;
+
+    use Dimension::*;
+    use GeoDataType::*;
     let geo_arr: Arc<dyn GeometryArrayTrait> = match data_type {
-        GeoDataType::Point(_, dim) => match dim {
-            Dimension::XY => Arc::new(PointArray::<2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(PointArray::<3>::try_from((array, field))?),
+        Point(_, dim) => match dim {
+            XY => Arc::new(PointArray::<2>::try_from((array, field))?),
+            XYZ => Arc::new(PointArray::<3>::try_from((array, field))?),
         },
-        GeoDataType::LineString(_, dim) => match dim {
-            Dimension::XY => Arc::new(LineStringArray::<i32, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(LineStringArray::<i32, 3>::try_from((array, field))?),
+        LineString(_, dim) => match dim {
+            XY => Arc::new(LineStringArray::<i32, 2>::try_from((array, field))?),
+            XYZ => Arc::new(LineStringArray::<i32, 3>::try_from((array, field))?),
         },
-        GeoDataType::LargeLineString(_, dim) => match dim {
-            Dimension::XY => Arc::new(LineStringArray::<i64, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(LineStringArray::<i64, 3>::try_from((array, field))?),
+        LargeLineString(_, dim) => match dim {
+            XY => Arc::new(LineStringArray::<i64, 2>::try_from((array, field))?),
+            XYZ => Arc::new(LineStringArray::<i64, 3>::try_from((array, field))?),
         },
-        GeoDataType::Polygon(_, dim) => match dim {
-            Dimension::XY => Arc::new(PolygonArray::<i32, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(PolygonArray::<i32, 3>::try_from((array, field))?),
+        Polygon(_, dim) => match dim {
+            XY => Arc::new(PolygonArray::<i32, 2>::try_from((array, field))?),
+            XYZ => Arc::new(PolygonArray::<i32, 3>::try_from((array, field))?),
         },
-        GeoDataType::LargePolygon(_, dim) => match dim {
-            Dimension::XY => Arc::new(PolygonArray::<i64, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(PolygonArray::<i64, 3>::try_from((array, field))?),
+        LargePolygon(_, dim) => match dim {
+            XY => Arc::new(PolygonArray::<i64, 2>::try_from((array, field))?),
+            XYZ => Arc::new(PolygonArray::<i64, 3>::try_from((array, field))?),
         },
-        GeoDataType::MultiPoint(_, dim) => match dim {
-            Dimension::XY => Arc::new(MultiPointArray::<i32, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MultiPointArray::<i32, 3>::try_from((array, field))?),
+        MultiPoint(_, dim) => match dim {
+            XY => Arc::new(MultiPointArray::<i32, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MultiPointArray::<i32, 3>::try_from((array, field))?),
         },
-        GeoDataType::LargeMultiPoint(_, dim) => match dim {
-            Dimension::XY => Arc::new(MultiPointArray::<i64, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MultiPointArray::<i64, 3>::try_from((array, field))?),
+        LargeMultiPoint(_, dim) => match dim {
+            XY => Arc::new(MultiPointArray::<i64, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MultiPointArray::<i64, 3>::try_from((array, field))?),
         },
-        GeoDataType::MultiLineString(_, dim) => match dim {
-            Dimension::XY => Arc::new(MultiLineStringArray::<i32, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MultiLineStringArray::<i32, 3>::try_from((array, field))?),
+        MultiLineString(_, dim) => match dim {
+            XY => Arc::new(MultiLineStringArray::<i32, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MultiLineStringArray::<i32, 3>::try_from((array, field))?),
         },
-        GeoDataType::LargeMultiLineString(_, dim) => match dim {
-            Dimension::XY => Arc::new(MultiLineStringArray::<i64, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MultiLineStringArray::<i64, 3>::try_from((array, field))?),
+        LargeMultiLineString(_, dim) => match dim {
+            XY => Arc::new(MultiLineStringArray::<i64, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MultiLineStringArray::<i64, 3>::try_from((array, field))?),
         },
-        GeoDataType::MultiPolygon(_, dim) => match dim {
-            Dimension::XY => Arc::new(MultiPolygonArray::<i32, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MultiPolygonArray::<i32, 3>::try_from((array, field))?),
+        MultiPolygon(_, dim) => match dim {
+            XY => Arc::new(MultiPolygonArray::<i32, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MultiPolygonArray::<i32, 3>::try_from((array, field))?),
         },
-        GeoDataType::LargeMultiPolygon(_, dim) => match dim {
-            Dimension::XY => Arc::new(MultiPolygonArray::<i64, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MultiPolygonArray::<i64, 3>::try_from((array, field))?),
+        LargeMultiPolygon(_, dim) => match dim {
+            XY => Arc::new(MultiPolygonArray::<i64, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MultiPolygonArray::<i64, 3>::try_from((array, field))?),
         },
-        GeoDataType::Mixed(_, dim) => match dim {
-            Dimension::XY => Arc::new(MixedGeometryArray::<i32, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MixedGeometryArray::<i32, 3>::try_from((array, field))?),
+        Mixed(_, dim) => match dim {
+            XY => Arc::new(MixedGeometryArray::<i32, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MixedGeometryArray::<i32, 3>::try_from((array, field))?),
         },
-        GeoDataType::LargeMixed(_, dim) => match dim {
-            Dimension::XY => Arc::new(MixedGeometryArray::<i64, 2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(MixedGeometryArray::<i64, 3>::try_from((array, field))?),
+        LargeMixed(_, dim) => match dim {
+            XY => Arc::new(MixedGeometryArray::<i64, 2>::try_from((array, field))?),
+            XYZ => Arc::new(MixedGeometryArray::<i64, 3>::try_from((array, field))?),
         },
-        GeoDataType::GeometryCollection(_, dim) => match dim {
-            Dimension::XY => Arc::new(GeometryCollectionArray::<i32, 2>::try_from((array, field))?),
-            Dimension::XYZ => {
-                Arc::new(GeometryCollectionArray::<i32, 3>::try_from((array, field))?)
-            }
+        GeometryCollection(_, dim) => match dim {
+            XY => Arc::new(GeometryCollectionArray::<i32, 2>::try_from((array, field))?),
+            XYZ => Arc::new(GeometryCollectionArray::<i32, 3>::try_from((array, field))?),
         },
-        GeoDataType::LargeGeometryCollection(_, dim) => match dim {
-            Dimension::XY => Arc::new(GeometryCollectionArray::<i64, 2>::try_from((array, field))?),
-            Dimension::XYZ => {
-                Arc::new(GeometryCollectionArray::<i64, 3>::try_from((array, field))?)
-            }
+        LargeGeometryCollection(_, dim) => match dim {
+            XY => Arc::new(GeometryCollectionArray::<i64, 2>::try_from((array, field))?),
+            XYZ => Arc::new(GeometryCollectionArray::<i64, 3>::try_from((array, field))?),
         },
-        GeoDataType::WKB => Arc::new(WKBArray::<i32>::try_from((array, field))?),
-        GeoDataType::LargeWKB => Arc::new(WKBArray::<i64>::try_from((array, field))?),
-        GeoDataType::Rect(dim) => match dim {
-            Dimension::XY => Arc::new(RectArray::<2>::try_from((array, field))?),
-            Dimension::XYZ => Arc::new(RectArray::<3>::try_from((array, field))?),
+        WKB => Arc::new(WKBArray::<i32>::try_from((array, field))?),
+        LargeWKB => Arc::new(WKBArray::<i64>::try_from((array, field))?),
+        Rect(dim) => match dim {
+            XY => Arc::new(RectArray::<2>::try_from((array, field))?),
+            XYZ => Arc::new(RectArray::<3>::try_from((array, field))?),
         },
     };
 
