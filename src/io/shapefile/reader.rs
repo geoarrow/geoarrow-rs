024bb93bbecc8944452d0e1e9038ef30aa84d42a
Kyle Barron <kyle@developmentseed.org>
2024-09-03 20:49:13 -0400

    wip

--- src/io/shapefile/reader.rs
+++ src/io/shapefile/reader.rs
@@ -1,17 +1,23 @@
 use std::io::{Read, Seek};
 use std::path::Path;
+use std::sync::Arc;
 
 use dbase::FieldType;
 use shapefile::{Reader, ShapeReader};
 
+use crate::array::PointBuilder;
 use crate::geo_traits::{
     CoordTrait, LineStringTrait, MultiLineStringTrait, MultiPointTrait, MultiPolygonTrait,
     PointTrait, PolygonTrait,
 };
+use crate::table::Table;
+use crate::trait_::GeometryArrayRef;
 
+// TODO:
+// stretch goal: return a record batch reader.
 pub fn read_shapefile<T: Read + Seek>(shp_reader: T, dbf_reader: T) {
     let dbf_reader = dbase::Reader::new(dbf_reader).unwrap();
-    let mut shp_reader = ShapeReader::new(shp_reader).unwrap();
+    let shp_reader = ShapeReader::new(shp_reader).unwrap();
 
     let header = shp_reader.header();
     // header.shape_type
@@ -26,7 +32,18 @@ pub fn read_shapefile<T: Read + Seek>(shp_reader: T, dbf_reader: T) {
     for x in reader.iter_shapes_and_records_as::<shapefile::Point, dbase::Record>() {
         let (geom, record) = x.unwrap();
         let y = Point(&geom);
+        PointBuilder::w
+        // record is a wrapper around a hash map of values
     }
+}
+
+fn read_point<T: Read + Seek, D: Read + Seek>(reader: &mut Reader<T, D>) -> GeometryArrayRef {
+    let mut builder = PointBuilder::<2>::with_capacity(reader.shape_count().unwrap());
+    for row in reader.iter_shapes_and_records_as::<shapefile::Point, dbase::Record>() {
+        let (geom, _record) = row.unwrap();
+        builder.push_point(Some(&Point(&geom)));
+    }
+    Arc::new(builder.finish())
 }
 
 struct Point<'a>(&'a shapefile::Point);
