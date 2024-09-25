c8c89a7d6aadfe5b45bfb81d170d592395873d3a
Kyle Barron <kylebarron2@gmail.com>
2024-09-24 21:59:51 -0400

    Use geo traits for parsing WKT

---
+++ src/io/wkt/reader/mod.rs
@@ -0,0 +1,33 @@
+use std::str::FromStr;
+use std::sync::Arc;
+
+use arrow_array::{Array, ArrayRef, GenericStringArray, OffsetSizeTrait};
+
+use crate::array::metadata::ArrayMetadata;
+use crate::array::{CoordType, MixedGeometryBuilder};
+use crate::NativeArray;
+
+mod wkt_trait;
+
+pub trait ParseWKT {
+    type Output;
+
+    fn parse_wkt(&self, coord_type: CoordType, metadata: Arc<ArrayMetadata>) -> Self::Output;
+}
+
+impl<O: OffsetSizeTrait> ParseWKT for GenericStringArray<O> {
+    type Output = Arc<dyn NativeArray>;
+
+    fn parse_wkt(&self, coord_type: CoordType, metadata: Arc<ArrayMetadata>) -> Self::Output {
+        let mut builder = MixedGeometryBuilder::<O, 2>::new_with_options(coord_type, metadata);
+        for i in 0..self.len() {
+            if self.is_valid(i) {
+                let w = wkt::Wkt::<f64>::from_str(self.value(i)).unwrap();
+                builder.push_geometry(Some(&w)).unwrap();
+            } else {
+                builder.push_null();
+            }
+        }
+        Arc::new(builder.finish())
+    }
+}
