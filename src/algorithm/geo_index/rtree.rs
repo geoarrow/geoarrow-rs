24582e5428a3d44650f78a03c0ddb34fc013d87e
Kyle Barron <kylebarron2@gmail.com>
2024-10-07 23:45:14 -0400

    Implement RTree for 3d arrays

--- src/algorithm/geo_index/rtree.rs
+++ src/algorithm/geo_index/rtree.rs
@@ -1,13 +1,12 @@
 use crate::algorithm::native::bounding_rect::{
     bounding_rect_geometry, bounding_rect_geometry_collection, bounding_rect_linestring,
     bounding_rect_multilinestring, bounding_rect_multipoint, bounding_rect_multipolygon,
-    bounding_rect_polygon, bounding_rect_rect,
+    bounding_rect_point, bounding_rect_polygon, bounding_rect_rect,
 };
 use crate::array::*;
 use crate::chunked_array::*;
 use crate::datatypes::{Dimension, NativeType};
-use crate::error::{GeoArrowError, Result};
-use crate::geo_traits::PointTrait;
+use crate::error::Result;
 use crate::trait_::ArrayAccessor;
 use crate::NativeArray;
 use geo_index::rtree::sort::HilbertSort;
@@ -23,41 +22,9 @@ pub trait RTree {
     fn create_rtree_with_node_size(&self, node_size: usize) -> Self::Output;
 }
 
-impl RTree for PointArray<2> {
-    type Output = OwnedRTree<f64>;
-
-    fn create_rtree_with_node_size(&self, node_size: usize) -> Self::Output {
-        assert_eq!(self.null_count(), 0);
-        let mut builder = RTreeBuilder::new_with_node_size(self.len(), node_size);
-
-        self.iter().flatten().for_each(|geom| {
-            let (x, y) = geom.x_y();
-            builder.add(x, y, x, y);
-        });
-
-        builder.finish::<HilbertSort>()
-    }
-}
-
-impl RTree for RectArray<2> {
-    type Output = OwnedRTree<f64>;
-
-    fn create_rtree_with_node_size(&self, node_size: usize) -> Self::Output {
-        assert_eq!(self.null_count(), 0);
-        let mut builder = RTreeBuilder::new_with_node_size(self.len(), node_size);
-
-        self.iter().flatten().for_each(|geom| {
-            let ([min_x, min_y], [max_x, max_y]) = bounding_rect_rect(&geom);
-            builder.add(min_x, min_y, max_x, max_y);
-        });
-
-        builder.finish::<HilbertSort>()
-    }
-}
-
 macro_rules! impl_rtree {
     ($struct_name:ty, $bounding_rect_fn:ident) => {
-        impl RTree for $struct_name {
+        impl<const D: usize> RTree for $struct_name {
             type Output = OwnedRTree<f64>;
 
             fn create_rtree_with_node_size(&self, node_size: usize) -> Self::Output {
@@ -75,16 +42,18 @@ macro_rules! impl_rtree {
     };
 }
 
-impl_rtree!(LineStringArray<2>, bounding_rect_linestring);
-impl_rtree!(PolygonArray<2>, bounding_rect_polygon);
-impl_rtree!(MultiPointArray<2>, bounding_rect_multipoint);
-impl_rtree!(MultiLineStringArray<2>, bounding_rect_multilinestring);
-impl_rtree!(MultiPolygonArray<2>, bounding_rect_multipolygon);
-impl_rtree!(MixedGeometryArray<2>, bounding_rect_geometry);
+impl_rtree!(PointArray<D>, bounding_rect_point);
+impl_rtree!(LineStringArray<D>, bounding_rect_linestring);
+impl_rtree!(PolygonArray<D>, bounding_rect_polygon);
+impl_rtree!(MultiPointArray<D>, bounding_rect_multipoint);
+impl_rtree!(MultiLineStringArray<D>, bounding_rect_multilinestring);
+impl_rtree!(MultiPolygonArray<D>, bounding_rect_multipolygon);
+impl_rtree!(MixedGeometryArray<D>, bounding_rect_geometry);
 impl_rtree!(
-    GeometryCollectionArray<2>,
+    GeometryCollectionArray<D>,
     bounding_rect_geometry_collection
 );
+impl_rtree!(RectArray<D>, bounding_rect_rect);
 
 impl RTree for &dyn NativeArray {
     type Output = OwnedRTree<f64>;
@@ -93,29 +62,32 @@ impl RTree for &dyn NativeArray {
         use Dimension::*;
         use NativeType::*;
 
+        macro_rules! impl_method {
+            ($method:ident, $dim:expr) => {
+                self.$method::<$dim>()
+                    .create_rtree_with_node_size(node_size)
+            };
+        }
+
         match self.data_type() {
-            Point(_, XY) => self.as_point::<2>().create_rtree_with_node_size(node_size),
-            LineString(_, XY) => self
-                .as_line_string::<2>()
-                .create_rtree_with_node_size(node_size),
-            Polygon(_, XY) => self
-                .as_polygon::<2>()
-                .create_rtree_with_node_size(node_size),
-            MultiPoint(_, XY) => self
-                .as_multi_point::<2>()
-                .create_rtree_with_node_size(node_size),
-            MultiLineString(_, XY) => self
-                .as_multi_line_string::<2>()
-                .create_rtree_with_node_size(node_size),
-            MultiPolygon(_, XY) => self
-                .as_multi_polygon::<2>()
-                .create_rtree_with_node_size(node_size),
-            Mixed(_, XY) => self.as_mixed::<2>().create_rtree_with_node_size(node_size),
-            GeometryCollection(_, XY) => self
-                .as_geometry_collection::<2>()
-                .create_rtree_with_node_size(node_size),
-            Rect(XY) => self.as_rect::<2>().create_rtree_with_node_size(node_size),
-            _ => todo!(),
+            Point(_, XY) => impl_method!(as_point, 2),
+            LineString(_, XY) => impl_method!(as_line_string, 2),
+            Polygon(_, XY) => impl_method!(as_polygon, 2),
+            MultiPoint(_, XY) => impl_method!(as_multi_point, 2),
+            MultiLineString(_, XY) => impl_method!(as_multi_line_string, 2),
+            MultiPolygon(_, XY) => impl_method!(as_multi_polygon, 2),
+            Mixed(_, XY) => impl_method!(as_mixed, 2),
+            GeometryCollection(_, XY) => impl_method!(as_geometry_collection, 2),
+            Rect(XY) => impl_method!(as_rect, 2),
+            Point(_, XYZ) => impl_method!(as_point, 3),
+            LineString(_, XYZ) => impl_method!(as_line_string, 3),
+            Polygon(_, XYZ) => impl_method!(as_polygon, 3),
+            MultiPoint(_, XYZ) => impl_method!(as_multi_point, 3),
+            MultiLineString(_, XYZ) => impl_method!(as_multi_line_string, 3),
+            MultiPolygon(_, XYZ) => impl_method!(as_multi_polygon, 3),
+            Mixed(_, XYZ) => impl_method!(as_mixed, 3),
+            GeometryCollection(_, XYZ) => impl_method!(as_geometry_collection, 3),
+            Rect(XYZ) => impl_method!(as_rect, 3),
         }
     }
 }
@@ -135,30 +107,33 @@ impl RTree for &dyn ChunkedNativeArray {
         use Dimension::*;
         use NativeType::*;
 
-        let result = match self.data_type() {
-            Point(_, XY) => self.as_point::<2>().create_rtree_with_node_size(node_size),
-            LineString(_, XY) => self
-                .as_line_string::<2>()
-                .create_rtree_with_node_size(node_size),
-            Polygon(_, XY) => self
-                .as_polygon::<2>()
-                .create_rtree_with_node_size(node_size),
-            MultiPoint(_, XY) => self
-                .as_multi_point::<2>()
-                .create_rtree_with_node_size(node_size),
-            MultiLineString(_, XY) => self
-                .as_multi_line_string::<2>()
-                .create_rtree_with_node_size(node_size),
-            MultiPolygon(_, XY) => self
-                .as_multi_polygon::<2>()
-                .create_rtree_with_node_size(node_size),
-            Mixed(_, XY) => self.as_mixed::<2>().create_rtree_with_node_size(node_size),
-            GeometryCollection(_, XY) => self
-                .as_geometry_collection::<2>()
-                .create_rtree_with_node_size(node_size),
-            _ => return Err(GeoArrowError::IncorrectType("".into())),
-        };
+        macro_rules! impl_method {
+            ($method:ident, $dim:expr) => {
+                self.$method::<$dim>()
+                    .create_rtree_with_node_size(node_size)
+            };
+        }
 
+        let result = match self.data_type() {
+            Point(_, XY) => impl_method!(as_point, 2),
+            LineString(_, XY) => impl_method!(as_line_string, 2),
+            Polygon(_, XY) => impl_method!(as_polygon, 2),
+            MultiPoint(_, XY) => impl_method!(as_multi_point, 2),
+            MultiLineString(_, XY) => impl_method!(as_multi_line_string, 2),
+            MultiPolygon(_, XY) => impl_method!(as_multi_polygon, 2),
+            Mixed(_, XY) => impl_method!(as_mixed, 2),
+            GeometryCollection(_, XY) => impl_method!(as_geometry_collection, 2),
+            Rect(XY) => impl_method!(as_rect, 2),
+            Point(_, XYZ) => impl_method!(as_point, 3),
+            LineString(_, XYZ) => impl_method!(as_line_string, 3),
+            Polygon(_, XYZ) => impl_method!(as_polygon, 3),
+            MultiPoint(_, XYZ) => impl_method!(as_multi_point, 3),
+            MultiLineString(_, XYZ) => impl_method!(as_multi_line_string, 3),
+            MultiPolygon(_, XYZ) => impl_method!(as_multi_polygon, 3),
+            Mixed(_, XYZ) => impl_method!(as_mixed, 3),
+            GeometryCollection(_, XYZ) => impl_method!(as_geometry_collection, 3),
+            Rect(XYZ) => impl_method!(as_rect, 3),
+        };
         Ok(result)
     }
 }
