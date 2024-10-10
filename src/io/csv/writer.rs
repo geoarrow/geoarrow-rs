99da6803d3d6eacb4e3471de671d72de6bc96ac5
Kyle Barron <kylebarron2@gmail.com>
2024-10-10 00:59:36 -0400

    lint

--- src/io/csv/writer.rs
+++ src/io/csv/writer.rs
@@ -31,7 +31,7 @@ fn encode_batch(batch: RecordBatch) -> Result<RecordBatch> {
     let mut new_columns = Vec::with_capacity(fields.len());
 
     for (field, column) in schema.fields().iter().zip(batch.columns()) {
-        if let Ok(arr) = NativeArrayDyn::from_arrow_array(&column, &field) {
+        if let Ok(arr) = NativeArrayDyn::from_arrow_array(&column, field) {
             let wkt_arr = arr.as_ref().to_wkt::<i32>();
             new_fields.push(wkt_arr.extension_field());
             new_columns.push(wkt_arr.into_array_ref());
