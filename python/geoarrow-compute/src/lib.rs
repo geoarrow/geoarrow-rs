fc2a563a19f1057870fce98e9f00158dbcb8f701
Kyle Barron <kylebarron2@gmail.com>
2024-11-13 10:56:11 -0500

    Python: Runtime warning when compiled in debug mode (#859)
    Closes https://github.com/geoarrow/geoarrow-rs/issues/851

--- python/geoarrow-compute/src/lib.rs
+++ python/geoarrow-compute/src/lib.rs
@@ -1,4 +1,7 @@
+use pyo3::exceptions::PyRuntimeWarning;
+use pyo3::intern;
 use pyo3::prelude::*;
+use pyo3::types::PyTuple;
 mod algorithm;
 pub mod broadcasting;
 pub mod ffi;
@@ -11,9 +14,25 @@ fn ___version() -> &'static str {
     VERSION
 }
 
+/// Raise RuntimeWarning for debug builds
+#[pyfunction]
+fn check_debug_build(py: Python) -> PyResult<()> {
+    #[cfg(debug_assertions)]
+    {
+        let warnings_mod = py.import_bound(intern!(py, "warnings"))?;
+        let warning = PyRuntimeWarning::new_err(
+            "geoarrow-rust-compute has not been compiled in release mode. Performance will be degraded.",
+        );
+        let args = PyTuple::new_bound(py, vec![warning.into_py(py)]);
+        warnings_mod.call_method1(intern!(py, "warn"), args)?;
+    }
+    Ok(())
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
-fn _compute(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
+fn _compute(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
+    check_debug_build(py)?;
     m.add_wrapped(wrap_pyfunction!(___version))?;
 
     // Top-level array/chunked array functions
