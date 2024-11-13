ab067f4d1ea2a951511a3ae07cef49c35bf6bc0a
Kyle Barron <kyle@developmentseed.org>
2024-11-13 00:41:34 -0500

    Finish

--- rust/geoarrow/src/io/wkb/mod.rs
+++ rust/geoarrow/src/io/wkb/mod.rs
@@ -1,9 +1,6 @@
 //! An optimized implementation of reading and writing ISO-flavored WKB-encoded geometries.
 
 mod api;
-mod common;
-// pub(crate) mod reader;
 pub(crate) mod writer;
 
 pub use api::{from_wkb, to_wkb, FromWKB, ToWKB};
-pub use common::WKBType;
