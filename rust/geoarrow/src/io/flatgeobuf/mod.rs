4458c215ef76218d836de87e8f6edf6a01f1e057
Nicholas Roberts <hydrogenplustime@gmail.com>
2025-02-13 02:11:02 +1100

    Fgb async iterator (#974)
    Supersedes https://github.com/geoarrow/geoarrow-rs/pull/966
    
    ---------
    
    Co-authored-by: Kyle Barron <kyle@developmentseed.org>
    Co-authored-by: Kyle Barron <kylebarron2@gmail.com>

--- rust/geoarrow/src/io/flatgeobuf/mod.rs
+++ rust/geoarrow/src/io/flatgeobuf/mod.rs
@@ -4,6 +4,6 @@ mod reader;
 mod writer;
 
 #[cfg(feature = "flatgeobuf_async")]
-pub use reader::read_flatgeobuf_async;
+pub use reader::{read_flatgeobuf_async, FlatGeobufStream, FlatGeobufStreamBuilder};
 pub use reader::{FlatGeobufReader, FlatGeobufReaderBuilder, FlatGeobufReaderOptions};
 pub use writer::{write_flatgeobuf, write_flatgeobuf_with_options, FlatGeobufWriterOptions};
