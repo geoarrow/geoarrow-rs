810592502c04570e46828e9830f3cb12d6c36c7d
Kyle Barron <kyle@developmentseed.org>
2025-04-03 00:14:16 -0400

    Remove CoordType default

--- rust/geoarrow-schema/src/coord_type.rs
+++ rust/geoarrow-schema/src/coord_type.rs
@@ -3,10 +3,9 @@
 /// GeoArrow permits coordinate types to either be `Interleaved`, where the X and Y coordinates are
 /// in a single buffer as XYXYXY or `Separated`, where the X and Y coordinates are in multiple
 /// buffers as XXXX and YYYY.
-#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum CoordType {
     /// Interleaved coordinates.
-    #[default]
     Interleaved,
 
     /// Separated coordinates.
