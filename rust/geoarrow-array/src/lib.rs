2685177882053f89143109760afa316e10bbf1b6
Kyle Barron <kyle@developmentseed.org>
2025-04-04 17:24:41 -0400

    Add top-level files

---
+++ rust/geoarrow-array/src/lib.rs
@@ -0,0 +1,66 @@
+//! Implementations of immutable GeoArrow arrays plus builders to more easily create arrays.
+//!
+//! There are three primary types of structs in this module: arrays, builders, and capacity
+//! counters.
+//!
+//! ## Arrays
+//!
+//! Arrays
+//!
+//! These arrays implement the binary layout defined in the [GeoArrow specification](https://github.com/geoarrow/geoarrow).
+//!
+//!
+//!
+//! These include:
+//!
+//! - [`PointArray`]
+//! - [`LineStringArray`]
+//! - [`PolygonArray`]
+//! - [`MultiPointArray`]
+//! - [`MultiLineStringArray`]
+//! - [`MultiPolygonArray`]
+//! - [`GeometryArray`]
+//! - [`GeometryCollectionArray`]
+//! - [`RectArray`]
+//!
+//! ## Builders
+//!
+//! Builders are designed to make it easier
+//!
+//! There's a builder for each of the above array types:
+//!
+//!
+//! - [`PointBuilder`]
+//! - [`LineStringBuilder`]
+//! - [`PolygonBuilder`]
+//! - [`MultiPointBuilder`]
+//! - [`MultiLineStringBuilder`]
+//! - [`MultiPolygonBuilder`]
+//! - [`GeometryBuilder`]
+//! - [`GeometryCollectionBuilder`]
+//! - [`RectBuilder`]
+//!
+//! Once you've finished adding geometries to a builder, it's `O(1)` to convert a builder to an
+//! array, by calling `finish()`.
+//!
+//! ## Capacity Counters
+//!
+//! Underlying the builders are growable `Vec`s. E.g. you can think of a `PointBuilder` as a buffer of `x` coordinates and a buffer of `y` coordinates.
+//!
+//! The fastest and most memory-efficient way to construct an array from a set of known geometries
+//! is to make a first pass over these geometries to count exactly how big each part of the Arrow
+//! array must be, allocate _once_ for exactly what you need, and then fill those buffers in a
+//! second pass.
+//!
+
+pub mod array;
+pub mod builder;
+pub mod capacity;
+pub mod cast;
+pub mod error;
+pub mod scalar;
+
+pub(crate) mod dynamic;
+pub(crate) mod offset_builder;
+mod trait_;
+pub(crate) mod util;
