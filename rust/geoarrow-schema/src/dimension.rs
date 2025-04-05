784cba96ec633480a9c8009f692989796388ab22
Dewey Dunnington <dewey@dunnington.ca>
2025-04-05 18:36:13 -0500

    Add additional tests for crs, dimension, and metadata modules (#1020)
    Mostly a productive way for me to learn how to use this properly. Happy
    to iterate if my Rust or GeoArrow usage is off!
    
    ---------
    
    Co-authored-by: Kyle Barron <kyle@developmentseed.org>

--- rust/geoarrow-schema/src/dimension.rs
+++ rust/geoarrow-schema/src/dimension.rs
@@ -93,3 +93,117 @@ impl TryFrom<geo_traits::Dimensions> for Dimension {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::iter::zip;
+
+    use arrow_schema::DataType;
+
+    use super::*;
+
+    #[test]
+    fn from_interleaved() {
+        assert!(matches!(
+            Dimension::from_interleaved_field(&Field::new("xy", DataType::Null, false)),
+            Dimension::XY
+        ));
+
+        assert!(matches!(
+            Dimension::from_interleaved_field(&Field::new("xyz", DataType::Null, false)),
+            Dimension::XYZ
+        ));
+
+        assert!(matches!(
+            Dimension::from_interleaved_field(&Field::new("xym", DataType::Null, false)),
+            Dimension::XYM
+        ));
+
+        assert!(matches!(
+            Dimension::from_interleaved_field(&Field::new("xyzm", DataType::Null, false)),
+            Dimension::XYZM
+        ));
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid interleaved field name: banana")]
+    fn from_bad_interleaved() {
+        Dimension::from_interleaved_field(&Field::new("banana", DataType::Null, false));
+    }
+
+    fn test_fields(dims: &[&str]) -> Fields {
+        dims.iter()
+            .map(|dim| Field::new(*dim, DataType::Null, false))
+            .collect()
+    }
+
+    #[test]
+    fn from_separated() {
+        assert!(matches!(
+            Dimension::from_separated_field(&test_fields(&["x", "y"])),
+            Dimension::XY
+        ));
+
+        assert!(matches!(
+            Dimension::from_separated_field(&test_fields(&["x", "y", "z"])),
+            Dimension::XYZ
+        ));
+
+        assert!(matches!(
+            Dimension::from_separated_field(&test_fields(&["x", "y", "m"])),
+            Dimension::XYM
+        ));
+
+        assert!(matches!(
+            Dimension::from_separated_field(&test_fields(&["x", "y", "z", "m"])),
+            Dimension::XYZM
+        ));
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid number of fields for separated coordinates: 1")]
+    fn from_bad_separated() {
+        Dimension::from_separated_field(&test_fields(&["x"]));
+    }
+
+    #[test]
+    fn geotraits_dimensions() {
+        let geoarrow_dims = [
+            Dimension::XY,
+            Dimension::XYZ,
+            Dimension::XYM,
+            Dimension::XYZM,
+        ];
+        let geotraits_dims = [
+            geo_traits::Dimensions::Xy,
+            geo_traits::Dimensions::Xyz,
+            geo_traits::Dimensions::Xym,
+            geo_traits::Dimensions::Xyzm,
+        ];
+
+        for (geoarrow_dim, geotraits_dim) in zip(geoarrow_dims, geotraits_dims) {
+            let into_geotraits_dim: geo_traits::Dimensions = geoarrow_dim.into();
+            assert_eq!(into_geotraits_dim, geotraits_dim);
+
+            let into_geoarrow_dim: Dimension = geotraits_dim.try_into().unwrap();
+            assert_eq!(into_geoarrow_dim, geoarrow_dim);
+
+            assert_eq!(geoarrow_dim.size(), geotraits_dim.size());
+        }
+
+        let dims2: Dimension = geo_traits::Dimensions::Unknown(2).try_into().unwrap();
+        assert_eq!(dims2, Dimension::XY);
+
+        let dims3: Dimension = geo_traits::Dimensions::Unknown(3).try_into().unwrap();
+        assert_eq!(dims3, Dimension::XYZ);
+
+        let dims4: Dimension = geo_traits::Dimensions::Unknown(4).try_into().unwrap();
+        assert_eq!(dims4, Dimension::XYZM);
+
+        let dims_err: Result<Dimension, ArrowError> = geo_traits::Dimensions::Unknown(0).try_into();
+        assert_eq!(
+            dims_err.unwrap_err().to_string(),
+            "Schema error: Unsupported dimension Unknown(0)"
+        );
+    }
+}
