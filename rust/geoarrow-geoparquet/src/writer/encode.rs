c4138834d155f7e8697e4f404abb863c830f035a
Kyle Barron <kyle@developmentseed.org>
2025-04-08 18:01:44 -0400

    Restore converting to WKB

--- rust/geoarrow-geoparquet/src/writer/encode.rs
+++ rust/geoarrow-geoparquet/src/writer/encode.rs
@@ -1,16 +1,14 @@
 use arrow_array::{Array, ArrayRef, RecordBatch};
 use arrow_schema::Field;
-use geoarrow_array::GeoArrowArray;
-use geoarrow_array::array::from_arrow_array;
+use geoarrow_array::array::{WKBArray, from_arrow_array};
+use geoarrow_array::builder::WKBBuilder;
+use geoarrow_array::cast::AsGeoArrowArray;
 use geoarrow_array::error::Result;
-use geoarrow_schema::CoordType;
+use geoarrow_array::{ArrayAccessor, GeoArrowArray, GeoArrowType};
+use geoarrow_schema::{CoordType, WkbType};
 
-use crate::algorithm::native::TotalBounds;
-use crate::algorithm::native::bounding_rect::BoundingRect;
-use crate::array::NativeArrayDyn;
-use crate::io::wkb::ToWKB;
 use crate::metadata::GeoParquetColumnEncoding;
-use crate::total_bounds::total_bounds;
+use crate::total_bounds::{BoundingRect, total_bounds};
 use crate::writer::metadata::{ColumnInfo, GeoParquetMetadataBuilder};
 
 pub(super) fn encode_record_batch(
@@ -50,13 +48,44 @@ fn encode_column(
 }
 
 /// Encode column as WKB
-fn encode_wkb_column(geo_arr: &dyn NativeArray) -> Result<ArrayRef> {
-    Ok(geo_arr.as_ref().to_wkb::<i32>().to_array_ref())
+fn encode_wkb_column(geo_arr: &dyn GeoArrowArray) -> Result<ArrayRef> {
+    Ok(to_wkb(geo_arr)?.to_array_ref())
 }
 
 /// Encode column as GeoArrow.
 ///
 /// Note that the GeoParquet specification requires separated coord type!
-fn encode_native_column(geo_arr: &dyn NativeArray) -> Result<ArrayRef> {
+fn encode_native_column(geo_arr: &dyn GeoArrowArray) -> Result<ArrayRef> {
     Ok(geo_arr.to_coord_type(CoordType::Separated).to_array_ref())
+}
+
+/// Convert to WKB
+fn to_wkb(arr: &dyn GeoArrowArray) -> Result<WKBArray<i32>> {
+    use GeoArrowType::*;
+    match arr.data_type() {
+        Point(_) => impl_to_wkb(arr.as_point()),
+        LineString(_) => impl_to_wkb(arr.as_line_string()),
+        Polygon(_) => impl_to_wkb(arr.as_polygon()),
+        MultiPoint(_) => impl_to_wkb(arr.as_multi_point()),
+        MultiLineString(_) => impl_to_wkb(arr.as_multi_line_string()),
+        MultiPolygon(_) => impl_to_wkb(arr.as_multi_polygon()),
+        Geometry(_) => impl_to_wkb(arr.as_geometry()),
+        GeometryCollection(_) => impl_to_wkb(arr.as_geometry_collection()),
+        Rect(_) => impl_to_wkb(arr.as_rect()),
+        WKB(_) => impl_to_wkb(arr.as_wkb()),
+        LargeWKB(_) => impl_to_wkb(arr.as_large_wkb()),
+        WKT(_) => todo!(),      // impl_to_wkb(arr.as_wkt()),
+        LargeWKT(_) => todo!(), // impl_to_wkb(arr.as_wkt()),
+    }
+}
+
+fn impl_to_wkb<'a>(geo_arr: &'a impl ArrayAccessor<'a>) -> Result<WKBArray<i32>> {
+    // let metadata = geo_arr.metadata().clone();
+
+    let geoms = geo_arr
+        .iter()
+        .map(|x| x.transpose())
+        .collect::<Result<Vec<_>>>()?;
+    let wkb_type = WkbType::new(Default::default());
+    Ok(WKBBuilder::from_nullable_geometries(geoms.as_slice(), wkb_type).finish())
 }
