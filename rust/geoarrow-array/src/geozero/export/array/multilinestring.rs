83459ce813b5020a1c5121f09252e9967f884d81
Kyle Barron <kyle@developmentseed.org>
2025-04-11 18:20:11 -0400

    multilinestring and multipolygon

--- rust/geoarrow-array/src/geozero/export/array/multilinestring.rs
+++ rust/geoarrow-array/src/geozero/export/array/multilinestring.rs
@@ -20,3 +20,24 @@ impl GeozeroGeometry for MultiLineStringArray {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use geoarrow_schema::{CoordType, Dimension, MultiLineStringType};
+    use geozero::ToWkt;
+
+    use crate::builder::MultiLineStringBuilder;
+    use crate::test::multilinestring::{ml0, ml1};
+
+    #[test]
+    fn geozero_process_geom() -> geozero::error::Result<()> {
+        let typ =
+            MultiLineStringType::new(CoordType::Interleaved, Dimension::XY, Default::default());
+        let geo_arr =
+            MultiLineStringBuilder::from_multi_line_strings(&[&ml0(), &ml1()], typ).finish();
+        let wkt = ToWkt::to_wkt(&geo_arr)?;
+        let expected = "GEOMETRYCOLLECTION(MULTILINESTRING((-111 45,-111 41,-104 41,-104 45)),MULTILINESTRING((-111 45,-111 41,-104 41,-104 45),(-110 44,-110 42,-105 42,-105 44)))";
+        assert_eq!(wkt, expected);
+        Ok(())
+    }
+}
--- rust/geoarrow-array/src/geozero/export/array/multipolygon.rs
+++ rust/geoarrow-array/src/geozero/export/array/multipolygon.rs
@@ -20,3 +20,22 @@ impl GeozeroGeometry for MultiPolygonArray {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use geoarrow_schema::{CoordType, Dimension, MultiPolygonType};
+    use geozero::ToWkt;
+
+    use crate::builder::MultiPolygonBuilder;
+    use crate::test::multipolygon::{mp0, mp1};
+
+    #[test]
+    fn geozero_process_geom() -> geozero::error::Result<()> {
+        let typ = MultiPolygonType::new(CoordType::Interleaved, Dimension::XY, Default::default());
+        let geo_arr = MultiPolygonBuilder::from_multi_polygons(&[&mp0(), &mp1()], typ).finish();
+        let wkt = ToWkt::to_wkt(&geo_arr)?;
+        let expected = "GEOMETRYCOLLECTION(MULTIPOLYGON(((-111 45,-111 41,-104 41,-104 45,-111 45)),((-111 45,-111 41,-104 41,-104 45,-111 45),(-110 44,-110 42,-105 42,-105 44,-110 44))),MULTIPOLYGON(((-111 45,-111 41,-104 41,-104 45,-111 45)),((-110 44,-110 42,-105 42,-105 44,-110 44))))";
+        assert_eq!(wkt, expected);
+        Ok(())
+    }
+}
