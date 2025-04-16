52909f07da74f9554630fea8f8595487e4f7c0b2
Kyle Barron <kylebarron2@gmail.com>
2025-04-16 15:58:05 -0400

    Restore `GeozeroDatasource` impl on Arrow `RecordBatchReader` (#1053)
    - This is necessary to write Arrow data to geozero-based consumers, like
    FlatGeobuf. AFAICT the [only way to write to
    FlatGeobuf](https://docs.rs/flatgeobuf/latest/flatgeobuf/struct.FgbWriter.html)
    in rust is via geozero.
    - This adds a dependency on `arrow-json` from `geoarrow-array`, but we
    can keep it optional and only used from the `geozero` module
    - Removes our own vendored copy of the json encoder.

---
+++ rust/geoarrow-array/src/geozero/export/data_source/record_batch_reader.rs
@@ -0,0 +1,44 @@
+use arrow_array::RecordBatchReader;
+
+/// A newtype wrapper around an [`arrow_array::RecordBatchReader`] so that we can implement the
+/// [`geozero::GeozeroDatasource`] trait on it.
+///
+/// This allows for exporting Arrow data to a geozero-based consumer even when not all of the Arrow
+/// data is present in memory at once.
+pub struct GeozeroRecordBatchReader(Box<dyn RecordBatchReader>);
+
+impl GeozeroRecordBatchReader {
+    /// Create a new GeozeroRecordBatchReader from a [`RecordBatchReader`].
+    pub fn new(reader: Box<dyn RecordBatchReader>) -> Self {
+        Self(reader)
+    }
+
+    /// Access the underlying [`RecordBatchReader`].
+    pub fn into_inner(self) -> Box<dyn RecordBatchReader> {
+        self.0
+    }
+}
+
+impl AsRef<Box<dyn RecordBatchReader>> for GeozeroRecordBatchReader {
+    fn as_ref(&self) -> &Box<dyn RecordBatchReader> {
+        &self.0
+    }
+}
+
+impl AsMut<Box<dyn RecordBatchReader>> for GeozeroRecordBatchReader {
+    fn as_mut(&mut self) -> &mut Box<dyn RecordBatchReader> {
+        &mut self.0
+    }
+}
+
+impl From<Box<dyn RecordBatchReader>> for GeozeroRecordBatchReader {
+    fn from(value: Box<dyn RecordBatchReader>) -> Self {
+        Self(value)
+    }
+}
+
+impl From<Box<dyn RecordBatchReader + Send>> for GeozeroRecordBatchReader {
+    fn from(value: Box<dyn RecordBatchReader + Send>) -> Self {
+        Self(value)
+    }
+}
