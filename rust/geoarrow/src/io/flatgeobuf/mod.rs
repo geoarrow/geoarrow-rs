0d730450b325d7228142a3617495fd7dd9030772
Kyle Barron <kyle@developmentseed.org>
2025-04-16 23:02:33 -0400

    Remove old writer code

--- rust/geoarrow/src/io/flatgeobuf/mod.rs
+++ rust/geoarrow/src/io/flatgeobuf/mod.rs
@@ -1,9 +1,7 @@
 //! Read from and write to [FlatGeobuf](https://flatgeobuf.org/) files.
 
 mod reader;
-mod writer;
 
 pub use reader::{FlatGeobufReader, FlatGeobufReaderBuilder, FlatGeobufReaderOptions};
 #[cfg(feature = "flatgeobuf_async")]
 pub use reader::{FlatGeobufStream, FlatGeobufStreamBuilder, read_flatgeobuf_async};
-pub use writer::{FlatGeobufWriterOptions, write_flatgeobuf, write_flatgeobuf_with_options};
