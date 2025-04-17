ad631be5b3cdbabdad80aeacb46a7e7cecdff48c
Kyle Barron <kyle@developmentseed.org>
2025-04-16 21:30:57 -0400

    Expose through top-level package

--- python/geoarrow-core/python/geoarrow/rust/core/types.py
+++ python/geoarrow-core/python/geoarrow/rust/core/types.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Literal, Union
+from typing import Union
 
 # The top-level import doesn't work for docs interlinking
 from pyproj.crs.crs import CRS
@@ -9,12 +9,3 @@ CRSInput = Union[CRS, str, dict, int]
 """Acceptable input for the CRS parameter."""
 
 IntFloat = Union[int, float]
-
-
-CoordTypeT = Literal["interleaved", "separated"]
-"""Acceptable coord_type strings.
-"""
-
-DimensionT = Literal["XY", "XYZ", "xy", "xyz"]
-"""Acceptable dimension strings.
-"""
