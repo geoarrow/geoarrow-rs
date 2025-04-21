ad2d29ef90050c5cfcfa7dfc0b4a3e5d12e51bbe
Kyle Barron <kylebarron2@gmail.com>
2025-04-21 18:20:00 -0400

    Add GeoParquet CRS tests for reading data (#1087)

--- rust/geoarrow-geoparquet/src/test/mod.rs
+++ rust/geoarrow-geoparquet/src/test/mod.rs
@@ -12,3 +12,7 @@ pub(crate) fn fixture_dir() -> PathBuf {
 pub(crate) fn geoarrow_data_example_files() -> PathBuf {
     fixture_dir().join("geoarrow-data/example/files")
 }
+
+pub(crate) fn geoarrow_data_example_crs_files() -> PathBuf {
+    fixture_dir().join("geoarrow-data/example-crs/files")
+}
