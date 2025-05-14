02e071c3e08a39d19784bde0b98e5944cf4b019e
Kyle Barron <kyle@developmentseed.org>
2025-05-14 12:24:26 -0400

    comment out node tests for now

--- js/tests/js/flatgeobuf.test.ts
+++ js/tests/js/flatgeobuf.test.ts
@@ -1,23 +1,23 @@
-import * as geoarrow from "../../pkg/node";
-import { tableFromIPC } from "apache-arrow";
-import { readFileSync } from "fs";
-import { expect, it } from "vitest";
+// import * as geoarrow from "../../pkg/node";
+// import { tableFromIPC } from "apache-arrow";
+// import { readFileSync } from "fs";
+// import { expect, it } from "vitest";
 
-geoarrow.set_panic_hook();
+// geoarrow.set_panic_hook();
 
-it("read FlatGeobuf", () => {
-  const path = "../fixtures/flatgeobuf/nz-building-outlines-small.fgb";
-  const buffer = new Uint8Array(readFileSync(path));
-  const wasmTable = geoarrow.readFlatGeobuf(buffer);
-  const arrowIPCBuffer = wasmTable.intoIPCStream();
-  const arrowJsTable = tableFromIPC(arrowIPCBuffer);
-  const geometryIdx = arrowJsTable.schema.fields.findIndex(
-    (field) => field.name === "geometry"
-  );
-  const geometryField = arrowJsTable.schema.fields[geometryIdx];
-  const geometryFieldMetadata = geometryField.metadata;
-  console.log(geometryFieldMetadata);
-  expect(geometryFieldMetadata.get("ARROW:extension:name")).toStrictEqual(
-    "geoarrow.multipolygon"
-  );
-});
+// it("read FlatGeobuf", () => {
+//   const path = "../fixtures/flatgeobuf/nz-building-outlines-small.fgb";
+//   const buffer = new Uint8Array(readFileSync(path));
+//   const wasmTable = geoarrow.readFlatGeobuf(buffer);
+//   const arrowIPCBuffer = wasmTable.intoIPCStream();
+//   const arrowJsTable = tableFromIPC(arrowIPCBuffer);
+//   const geometryIdx = arrowJsTable.schema.fields.findIndex(
+//     (field) => field.name === "geometry"
+//   );
+//   const geometryField = arrowJsTable.schema.fields[geometryIdx];
+//   const geometryFieldMetadata = geometryField.metadata;
+//   console.log(geometryFieldMetadata);
+//   expect(geometryFieldMetadata.get("ARROW:extension:name")).toStrictEqual(
+//     "geoarrow.multipolygon"
+//   );
+// });
