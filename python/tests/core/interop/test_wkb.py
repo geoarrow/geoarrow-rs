11ba9350b0aa7897bb6ec2667bfb7c94528b0284
Kyle Barron <kyle@developmentseed.org>
2025-06-12 15:58:09 -0400

    Add wkb and wkt tests

--- python/tests/core/interop/test_wkb.py
+++ python/tests/core/interop/test_wkb.py
@@ -10,6 +10,7 @@ from geoarrow.rust.core import (
     get_crs,
     multipolygon,
     point,
+    to_wkb,
     wkb,
 )
 from pyproj import CRS
@@ -18,46 +19,58 @@ from pyproj import CRS
 def test_parse_points():
     geoms = shapely.points([1, 2], [4, 5])
     wkb_geoms = pa.array(shapely.to_wkb(geoms))
-    parsed = from_wkb(wkb_geoms)
-    assert parsed.type == geometry()
 
-    parsed = from_wkb(wkb_geoms, point("xy"))
-    assert parsed.type == point("xy")
+    parsed1 = from_wkb(wkb_geoms)
+    assert parsed1.type == geometry()
+
+    parsed2 = from_wkb(wkb_geoms, point("xy"))
+    assert parsed2.type == point("xy")
+
+    assert wkb_geoms == to_wkb(parsed1)
+    assert wkb_geoms == to_wkb(parsed2)
 
 
 def test_parse_nybb():
     gdf = gpd.read_file(geodatasets.get_path("ny.bb"))
     wkb_geoms = pa.array(gdf.geometry.to_wkb())
-    parsed = from_wkb(wkb_geoms)
-    assert parsed.type == geometry()
+    parsed1 = from_wkb(wkb_geoms)
+    assert parsed1.type == geometry()
 
-    parsed = from_wkb(wkb_geoms, multipolygon("xy"))
-    assert parsed.type == multipolygon("xy")
+    parsed2 = from_wkb(wkb_geoms, multipolygon("xy"))
+    assert parsed2.type == multipolygon("xy")
 
-    parsed = from_wkb(wkb_geoms, wkb())
-    assert parsed.type == wkb()
+    parsed3 = from_wkb(wkb_geoms, wkb())
+    assert parsed3.type == wkb()
 
-    parsed = from_wkb(wkb_geoms, wkb(crs="EPSG:4326"))
-    assert get_crs(parsed) == CRS("EPSG:4326")
-    assert parsed.type == wkb(crs="EPSG:4326")
+    parsed4 = from_wkb(wkb_geoms, wkb(crs="EPSG:4326"))
+    assert get_crs(parsed4) == CRS("EPSG:4326")
+    assert parsed4.type == wkb(crs="EPSG:4326")
+
+    assert wkb_geoms == to_wkb(parsed1)
+    assert wkb_geoms == to_wkb(parsed2)
+    assert wkb_geoms == to_wkb(parsed3)
 
 
 def test_parse_nybb_chunked():
     gdf = gpd.read_file(geodatasets.get_path("ny.bb"))
     wkb_geoms = pa.array(gdf.geometry.to_wkb())
     wkb_ca = pa.chunked_array([wkb_geoms, wkb_geoms])
-    parsed = from_wkb(wkb_ca)
-    assert isinstance(parsed, GeoArrayReader)
-    assert parsed.type == geometry()
+
+    parsed1 = from_wkb(wkb_ca)
+    assert isinstance(parsed1, GeoArrayReader)
+    assert parsed1.type == geometry()
 
     # Note that this works to parse a MultiPolygon as a point because no parsing has
     # happened yet; it's lazy
-    parsed = from_wkb(wkb_ca, point("xy"))
+    parsed2 = from_wkb(wkb_ca, point("xy"))
     with pytest.raises(Exception, match="Incorrect geometry type for operation"):
-        parsed.read_next_array()
+        parsed2.read_next_array()
 
-    parsed = from_wkb(wkb_ca, multipolygon("xy"))
-    assert isinstance(parsed, GeoArrayReader)
-    assert parsed.type == multipolygon("xy")
-    geo_chunked_array = parsed.read_all()
+    parsed3 = from_wkb(wkb_ca, multipolygon("xy"))
+    assert isinstance(parsed3, GeoArrayReader)
+    assert parsed3.type == multipolygon("xy")
+    geo_chunked_array = parsed3.read_all()
     assert geo_chunked_array.type == multipolygon("xy")
+
+    assert wkb_ca == to_wkb(from_wkb(wkb_ca)).read_all()
+    assert wkb_ca == to_wkb(from_wkb(wkb_ca, multipolygon("xy"))).read_all()
