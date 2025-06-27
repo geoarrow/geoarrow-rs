1d83cacc24b92ae8b37f5d94f16dc74437a15c86
Kyle Barron <kylebarron2@gmail.com>
2025-06-27 17:14:09 -0400

    feat(geoarrow-geo): Initial geoarrow-geo crate using `ArrayAccessor` trait (#1025)
    ### Highlights
    
    - Starts `geoarrow-geo` crate.
    - Pure function might provide simpler implementation than trait methods.
        
    
    [`arrow::compute`](https://docs.rs/arrow/latest/arrow/compute/index.html)
    exclusively uses pure functions and not traits, so this might be a good
    design decision to copy. I think the main reason why we didn't use pure
    functions before was that we wanted to apply the same name onto both
    `Array` and `ChunkedArray`. But at this point I think it's better to
    leave chunked arrays out of `geoarrow-rs` entirely and leave
    chunked/streaming operations to datafusion.
    - 
    
    - This defers to the `geo::Area`
    [implementation](https://docs.rs/geo/latest/geo/algorithm/area/trait.Area.html#tymethod.unsigned_area).
    Note that it uses the upstream implementation [on
    `Geometry`](https://docs.rs/geo/latest/geo/algorithm/area/trait.Area.html#impl-Area%3CT%3E-for-Geometry%3CT%3E).
    So we don't have to handle the switch on geometry type ourselves because
    `geo` will do it for us.
    - However, not every geo algorithm is defined on `Geometry`. For
    example,
    [simplify](https://docs.rs/geo/latest/geo/algorithm/simplify/trait.Simplify.html#implementors)
    is defined on lines and polygons but not an arbitrary `Geometry`. This
    means that we'd still have to manage downcasting from `Geometry`
    ourselves.
    - I added a quick example of wrapping simplify. It's still a bit verbose
    because we need to figure out what array type to return.
    - Shows an example of "specialization". If we know that some geometry
    types can have a fast path, then

---
+++ rust/geoarrow-geo/src/convex_hull.rs
@@ -0,0 +1,30 @@
+use geo::ConvexHull;
+use geo_traits::to_geo::ToGeoGeometry;
+use geoarrow_array::array::PolygonArray;
+use geoarrow_array::builder::PolygonBuilder;
+use geoarrow_array::{GeoArrowArray, GeoArrowArrayAccessor, downcast_geoarrow_array};
+use geoarrow_schema::error::GeoArrowResult;
+use geoarrow_schema::{Dimension, PolygonType};
+
+pub fn convex_hull(array: &dyn GeoArrowArray) -> GeoArrowResult<PolygonArray> {
+    downcast_geoarrow_array!(array, convex_hull_impl)
+}
+
+fn convex_hull_impl<'a>(array: &'a impl GeoArrowArrayAccessor<'a>) -> GeoArrowResult<PolygonArray> {
+    let coord_type = array.data_type().coord_type().unwrap_or_default();
+    let typ = PolygonType::new(Dimension::XY, array.data_type().metadata().clone())
+        .with_coord_type(coord_type);
+    let mut builder = PolygonBuilder::new(typ);
+
+    for item in array.iter() {
+        if let Some(geom) = item {
+            let geo_geom = geom?.to_geometry();
+            let poly = geo_geom.convex_hull();
+            builder.push_polygon(Some(&poly))?;
+        } else {
+            builder.push_polygon(None::<geo::Polygon>.as_ref())?;
+        }
+    }
+
+    Ok(builder.finish())
+}
