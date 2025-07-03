01185aa763d5b96fe4783b98638bb7069700a483
Kyle Barron <kyle@developmentseed.org>
2025-07-03 13:19:12 -0400

    refactor metadata to validate that geometry columns exist on input

--- rust/geoparquet/src/writer/metadata.rs
+++ rust/geoparquet/src/writer/metadata.rs
@@ -21,34 +21,34 @@ use crate::writer::options::{GeoParquetWriterEncoding, GeoParquetWriterOptions};
 const INFERRED_PRIMARY_COLUMN_NAMES: [&str; 2] = ["geometry", "geography"];
 
 /// Information for one geometry column being written to Parquet
-pub struct ColumnInfo {
+pub(crate) struct ColumnInfo {
     /// The name of this geometry column
-    pub name: String,
+    pub(crate) name: String,
 
     /// The serialized encoding for this geometry column.
-    pub encoding: GeoParquetColumnEncoding,
+    pub(crate) encoding: GeoParquetColumnEncoding,
 
     /// The set of string geometry types for this geometry column
-    pub geometry_types: HashSet<GeoParquetGeometryTypeAndDimension>,
+    pub(crate) geometry_types: HashSet<GeoParquetGeometryTypeAndDimension>,
 
     /// The bounding box of this column.
-    pub bbox: Option<BoundingRect>,
+    pub(crate) bbox: Option<BoundingRect>,
 
     /// The PROJJSON CRS for this geometry column.
-    pub crs: Option<Value>,
+    pub(crate) crs: Option<Value>,
 
     /// If present, instructs consumers that edges follow a spherical path rather than a planar
     /// one. If this value is omitted, edges will be interpreted as planar.
-    pub edges: Option<Edges>,
+    pub(crate) edges: Option<Edges>,
 }
 
 impl ColumnInfo {
     #[allow(clippy::borrowed_box)]
-    pub fn try_new(
+    pub(crate) fn try_new(
         name: String,
         writer_encoding: GeoParquetWriterEncoding,
         data_type: &GeoArrowType,
-        metadata: Metadata,
+        metadata: &Metadata,
         crs_transform: Option<&Box<dyn CrsTransform>>,
     ) -> GeoArrowResult<Self> {
         let encoding = GeoParquetColumnEncoding::try_new(writer_encoding, data_type)?;
@@ -71,7 +71,7 @@ impl ColumnInfo {
         })
     }
 
-    pub fn update_bbox(&mut self, new_bounds: &BoundingRect) {
+    pub(crate) fn update_bbox(&mut self, new_bounds: &BoundingRect) {
         if let Some(existing_bounds) = self.bbox.as_mut() {
             existing_bounds.update(new_bounds)
         } else {
@@ -87,7 +87,11 @@ impl ColumnInfo {
     // Note: for these multi columns, we should first check the geometry_types HashSet, because we
     // shouldn't compute that for every array if we see in the first that the data is both multi
     // and single polygons.
-    pub fn update_geometry_types(&mut self, array: &ArrayRef, field: &Field) -> GeoArrowResult<()> {
+    pub(crate) fn update_geometry_types(
+        &mut self,
+        array: &ArrayRef,
+        field: &Field,
+    ) -> GeoArrowResult<()> {
         let array = from_arrow_array(array, field)?;
         let array_ref = array.as_ref();
 
@@ -129,7 +133,7 @@ impl ColumnInfo {
     }
 
     /// Returns (column_name, column_metadata)
-    pub fn finish(self) -> (String, GeoParquetColumnMetadata) {
+    pub(crate) fn finish(self) -> (String, GeoParquetColumnMetadata) {
         let edges = self.edges.and_then(|edges| match edges {
             Edges::Spherical => Some("spherical".to_string()),
             _ => None,
@@ -164,14 +168,17 @@ impl ColumnInfo {
     }
 }
 
-pub struct GeoParquetMetadataBuilder {
-    pub output_schema: SchemaRef,
-    pub primary_column: Option<String>,
-    pub columns: HashMap<usize, ColumnInfo>,
+pub(crate) struct GeoParquetMetadataBuilder {
+    pub(crate) output_schema: SchemaRef,
+    pub(crate) primary_column: String,
+    pub(crate) columns: HashMap<usize, ColumnInfo>,
 }
 
 impl GeoParquetMetadataBuilder {
-    pub fn try_new(schema: &Schema, options: &GeoParquetWriterOptions) -> GeoArrowResult<Self> {
+    pub(crate) fn try_new(
+        schema: &Schema,
+        options: &GeoParquetWriterOptions,
+    ) -> GeoArrowResult<Self> {
         let mut columns = HashMap::new();
 
         for (col_idx, field) in schema.fields().iter().enumerate() {
@@ -181,22 +188,27 @@ impl GeoParquetMetadataBuilder {
                 }
 
                 let column_name = schema.field(col_idx).name().clone();
-
-                let metadata = Metadata::try_from(field.as_ref())?;
                 let geo_data_type = field.as_ref().try_into()?;
 
                 let column_info = ColumnInfo::try_new(
                     column_name,
                     options.encoding,
                     &geo_data_type,
-                    metadata,
+                    geo_data_type.metadata(),
                     options.crs_transform.as_ref(),
                 )?;
 
                 columns.insert(col_idx, column_info);
             }
         }
-        if let Some(primary_column) = options.primary_column.as_deref() {
+
+        if columns.is_empty() {
+            return Err(GeoArrowError::GeoParquet(
+                "No geometry columns found in schema".to_string(),
+            ));
+        }
+
+        let primary_column = if let Some(primary_column) = options.primary_column.as_deref() {
             if !columns
                 .values()
                 .any(|column_info| column_info.name == primary_column)
@@ -205,10 +217,28 @@ impl GeoParquetMetadataBuilder {
                     "Designated primary column: {primary_column} does not exist as a geometry field in schema",
                 )));
             }
-        }
+
+            primary_column.to_string()
+        } else if let Some(column_info) = columns
+            .values()
+            .find(|column_info| INFERRED_PRIMARY_COLUMN_NAMES.contains(&column_info.name.as_str()))
+        {
+            column_info.name.clone()
+        } else {
+            // Make it deterministic which key we use.
+            let mut keys: Vec<_> = columns.keys().collect();
+
+            // We already checked for empty columns
+            assert!(!keys.is_empty());
+            keys.sort();
+            keys.get(0)
+                .expect("No geometry columns when finishing GeoParquetMetadataBuilder")
+                .to_string()
+        };
+
         let output_schema = create_output_schema(schema, &columns);
         Ok(Self {
-            primary_column: options.primary_column.clone(),
+            primary_column,
             columns,
             output_schema,
         })
@@ -224,40 +254,25 @@ impl GeoParquetMetadataBuilder {
         }
     }
 
-    pub fn finish(self) -> Option<GeoParquetMetadata> {
+    /// Consume this builder, converting into [GeoParquetMetadata].
+    pub(crate) fn finish(self) -> GeoParquetMetadata {
         let mut columns = HashMap::with_capacity(self.columns.len());
         for column_info in self.columns.into_values() {
             let (column_name, column_meta) = column_info.finish();
             columns.insert(column_name, column_meta);
         }
 
-        if columns.is_empty() {
-            None
-        } else {
-            let primary_column = if let Some(primary_column) = self.primary_column {
-                primary_column
-            } else if let Some(column_name) = columns
-                .keys()
-                .find(|column_name| INFERRED_PRIMARY_COLUMN_NAMES.contains(&column_name.as_str()))
-            {
-                column_name.clone()
-            } else {
-                // Make it deterministic which key we use.
-                let mut keys: Vec<_> = columns.keys().collect();
-                assert!(!keys.is_empty()); // We already checked for empty columns
-                keys.sort();
-                keys[0].to_string()
-            };
-            Some(GeoParquetMetadata {
-                version: "1.1.0".to_string(),
-                primary_column,
-                columns,
-            })
+        GeoParquetMetadata {
+            version: "1.1.0".to_string(),
+            primary_column: self.primary_column,
+            columns,
         }
     }
 }
 
-pub fn get_geometry_types(data_type: &GeoArrowType) -> HashSet<GeoParquetGeometryTypeAndDimension> {
+pub(crate) fn get_geometry_types(
+    data_type: &GeoArrowType,
+) -> HashSet<GeoParquetGeometryTypeAndDimension> {
     use GeoParquetGeometryType::*;
     let mut geometry_types = HashSet::new();
 
@@ -371,12 +386,7 @@ mod tests {
     fn primary_column_none_no_geometry() {
         let schema = Schema::empty();
         let options = GeoParquetWriterOptions::default();
-        assert!(
-            GeoParquetMetadataBuilder::try_new(&schema, &options)
-                .unwrap()
-                .finish()
-                .is_none()
-        );
+        assert!(GeoParquetMetadataBuilder::try_new(&schema, &options).is_err());
     }
 
     #[test]
@@ -386,8 +396,7 @@ mod tests {
         let options = GeoParquetWriterOptions::default();
         let metadata = GeoParquetMetadataBuilder::try_new(&schema, &options)
             .unwrap()
-            .finish()
-            .unwrap();
+            .finish();
         assert_eq!(metadata.primary_column, "anything");
     }
 
@@ -399,8 +408,7 @@ mod tests {
         let options = GeoParquetWriterOptions::default();
         let metadata = GeoParquetMetadataBuilder::try_new(&schema, &options)
             .unwrap()
-            .finish()
-            .unwrap();
+            .finish();
         assert_eq!(metadata.primary_column, "geometry");
     }
 
@@ -415,8 +423,7 @@ mod tests {
         };
         let metadata = GeoParquetMetadataBuilder::try_new(&schema, &options)
             .unwrap()
-            .finish()
-            .unwrap();
+            .finish();
         assert_eq!(metadata.primary_column, "anything");
     }
 }
