ad43295bd49a868d3a02bdf0f98867b51a786ea6
Kyle Barron <kyle@developmentseed.org>
2025-07-15 16:34:37 -0400

    Remove old code

--- rust/geoarrow-old/src/io/flatgeobuf/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Read from and write to [FlatGeobuf](https://flatgeobuf.org/) files.
-
-mod reader;
-
-pub use reader::{FlatGeobufReader, FlatGeobufReaderBuilder, FlatGeobufReaderOptions};
-#[cfg(feature = "flatgeobuf_async")]
-pub use reader::{FlatGeobufStream, FlatGeobufStreamBuilder, read_flatgeobuf_async};
