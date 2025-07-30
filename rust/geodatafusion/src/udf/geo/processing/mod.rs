97b55fefa6d2161fe48b1e4b7b3a5a6c81787a1e
Kyle Barron <kyle@developmentseed.org>
2025-07-30 16:02:07 -0400

    feat(geodatafusion): ST_OrientedEnvelope

--- rust/geodatafusion/src/udf/geo/processing/mod.rs
+++ rust/geodatafusion/src/udf/geo/processing/mod.rs
@@ -1,7 +1,9 @@
 mod centroid;
 mod convex_hull;
+mod oriented_envelope;
 mod point_on_surface;
 
 pub use centroid::Centroid;
 pub use convex_hull::ConvexHull;
+pub use oriented_envelope::OrientedEnvelope;
 pub use point_on_surface::PointOnSurface;
