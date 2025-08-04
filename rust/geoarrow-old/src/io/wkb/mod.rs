69428b07eea67d76cf0c32958abd04a94372cf5a
Kyle Barron <kylebarron2@gmail.com>
2025-08-04 11:10:26 -0400

    chore(geoarrow-old): Remove more code from old, unused `geoarrow` crate (#1265)

--- rust/geoarrow-old/src/io/wkb/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Read and write geometries encoded as [Well-Known Binary](https://libgeos.org/specifications/wkb/).
-//!
-//! This wraps the [wkb] crate. As such, it currently supports reading the ISO and extended (EWKB)
-//! variants of WKB. Currently, it always writes the ISO WKB variant.
-
-mod api;
-pub(crate) mod writer;
-
-pub use api::{FromWKB, ToWKB, from_wkb, to_wkb};
--- rust/geoarrow-old/src/io/wkb/writer/mod.rs
+++
@@ -1,9 +0,0 @@
-mod geometry;
-mod geometrycollection;
-mod linestring;
-mod multilinestring;
-mod multipoint;
-mod multipolygon;
-mod point;
-mod polygon;
-mod rect;
