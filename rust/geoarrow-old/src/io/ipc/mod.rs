8b8f6882ed57d66a66ed387105439c8d904c88ee
Kyle Barron <kyle@developmentseed.org>
2025-08-04 11:06:04 -0400

    chore(geoarrow-old): Remove more code from old, unused `geoarrow` crate

--- rust/geoarrow-old/src/io/ipc/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Read from and write to [Arrow IPC](https://arrow.apache.org/docs/format/Columnar.html#format-ipc) files.
-
-mod reader;
-mod writer;
-
-pub use reader::{read_ipc, read_ipc_stream};
-pub use writer::{write_ipc, write_ipc_stream};
