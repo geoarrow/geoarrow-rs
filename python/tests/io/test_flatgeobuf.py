fa1b7c1378bac9e8256fcddc629a9a9c70f5ec64
Kyle Barron <kyle@developmentseed.org>
2025-08-06 13:04:03 -0400

    Add test

--- python/tests/io/test_flatgeobuf.py
+++ python/tests/io/test_flatgeobuf.py
@@ -1,23 +1,37 @@
 from tempfile import TemporaryDirectory
 
 import pyogrio
-from geoarrow.rust.io import write_flatgeobuf
+from geoarrow.rust.io import read_flatgeobuf
+# from geoarrow.rust.io import write_flatgeobuf
 
 from tests.utils import FIXTURES_DIR
 
 
-def test_write_flatgeobuf_with_wkb_geometry():
+def test_read_flatgeobuf():
     path = FIXTURES_DIR / "flatgeobuf" / "countries.fgb"
-    # pyogrio.read_arrow leaves the geometry as WKB when reading.
-    (meta, table) = pyogrio.read_arrow(path)
+    table = read_flatgeobuf(path)
+    pyogrio.read_arrow(path)
 
-    field_meta = table.schema.field("wkb_geometry").metadata
-    assert field_meta[b"ARROW:extension:name"] == b"geoarrow.wkb"
+    assert table.num_rows == 179
+    assert table.num_columns == 3
+    assert (
+        table.schema.field("geometry").metadata_str["ARROW:extension:name"]
+        == "geoarrow.multipolygon"
+    )
 
-    with TemporaryDirectory() as tmpdir:
-        tmp_path = f"{tmpdir}/countries.fgb"
-        write_flatgeobuf(table, tmp_path, write_index=False)
 
-        (meta2, table2) = pyogrio.read_arrow(path)
+# def test_write_flatgeobuf_with_wkb_geometry():
+#     path = FIXTURES_DIR / "flatgeobuf" / "countries.fgb"
+#     # pyogrio.read_arrow leaves the geometry as WKB when reading.
+#     (meta, table) = pyogrio.read_arrow(path)
 
-    assert table == table2
+#     field_meta = table.schema.field("wkb_geometry").metadata
+#     assert field_meta[b"ARROW:extension:name"] == b"geoarrow.wkb"
+
+#     with TemporaryDirectory() as tmpdir:
+#         tmp_path = f"{tmpdir}/countries.fgb"
+#         write_flatgeobuf(table, tmp_path, write_index=False)
+
+#         (meta2, table2) = pyogrio.read_arrow(path)
+
+#     assert table == table2
