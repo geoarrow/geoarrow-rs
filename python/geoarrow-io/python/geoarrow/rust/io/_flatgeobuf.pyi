d739ab3310b47a4ff104a1b6adf00b0df4f57279
Kyle Barron <kyle@developmentseed.org>
2025-08-06 14:06:59 -0400

    Update docs

--- python/geoarrow-io/python/geoarrow/rust/io/_flatgeobuf.pyi
+++ python/geoarrow-io/python/geoarrow/rust/io/_flatgeobuf.pyi
@@ -15,8 +15,8 @@ def read_flatgeobuf(
     batch_size: int = 65536,
     bbox: Tuple[int | float, int | float, int | float, int | float] | None = None,
     coord_type: CoordTypeInput | None = None,
-    prefer_view_types: bool = True,
-    max_read_records: int | None = 1000,
+    use_view_types: bool = True,
+    max_scan_records: int | None = 1000,
     read_geometry: bool = True,
 ) -> Table:
     """
@@ -24,54 +24,70 @@ def read_flatgeobuf(
 
     Example:
 
-    Reading from a local path:
-
-    ```py
-    from geoarrow.rust.io import read_flatgeobuf
-    table = read_flatgeobuf("path/to/file.fgb")
-    ```
-
-    Reading from a Python file object:
-
-    ```py
-    from geoarrow.rust.io import read_flatgeobuf
-
-    with open("path/to/file.fgb", "rb") as file:
-        table = read_flatgeobuf(file)
-    ```
-
-    Reading from an HTTP(S) url:
-
-    ```py
-    from geoarrow.rust.io import read_flatgeobuf
-
-    url = "http://flatgeobuf.org/test/data/UScounties.fgb"
-    table = read_flatgeobuf(url)
-    ```
-
-    Reading from a remote file on an S3 bucket.
-
-    ```py
-    from geoarrow.rust.io import ObjectStore, read_flatgeobuf
-
-    options = {
-        "aws_access_key_id": "...",
-        "aws_secret_access_key": "...",
-        "aws_region": "..."
-    }
-    store = ObjectStore('s3://bucket', options=options)
-    table = read_flatgeobuf("path/in/bucket.fgb", store=store)
-    ```
-
     Args:
-        file: the path to the file or a Python file object in binary read mode.
+        path: the path to the file or a Python file object in binary read mode.
 
-    Other args:
-        store: an ObjectStore instance for this url. This is required only if the file is at a remote
-            location.
+    Other Args:
+        store: an ObjectStore instance for this url. This is required only if the file
+            is at a remote location and if the store cannot be inferred.
         batch_size: the number of rows to include in each internal batch of the table.
-        bbox: A spatial filter for reading rows, of the format (minx, miny, maxx, maxy).
-            If set to `None`, no spatial filtering will be performed.
+        bbox: A spatial filter for reading rows, of the format `(minx, miny, maxx,
+            maxy)`. If set to `None`, no spatial filtering will be performed.
+        coord_type: The GeoArrow coordinate type to use for generated geometries. The default is to use "separated" coordinates.
+        use_view_types: If `True`, load string and binary columns into Arrow string view and binary view data types. These are more efficient but less widely supported than the older string and binary data types. Defaults to `True`.
+        max_scan_records: The maximum number of records to scan for schema inference. If set to `None`, all records will be scanned. Defaults to 1000.
+
+            Most FlatGeobuf files have a schema defined in the header metadata. But for
+            files that do not have a known schema, we need to scan some initial records
+            to infer a schema. Reading will fail if a new property with an unknown name
+            is found that was not in the schema. Thus, scanning fewer records will be
+            faster, but could fail later if the inferred schema was not complete.
+        read_geometry: If `True`, read the geometry column. If `False`, the geometry column will be omitted from the result. Defaults to `True`.
+
+    Examples:
+        Reading from a local path:
+
+        ```py
+        from geoarrow.rust.io import read_flatgeobuf
+        table = read_flatgeobuf("path/to/file.fgb")
+        ```
+
+        Reading from a Python file object:
+
+        ```py
+        from geoarrow.rust.io import read_flatgeobuf
+
+        with open("path/to/file.fgb", "rb") as file:
+            table = read_flatgeobuf(file)
+        ```
+
+        Reading from an HTTP(S) url:
+
+        ```py
+        from geoarrow.rust.io import read_flatgeobuf
+
+        url = "http://flatgeobuf.org/test/data/UScounties.fgb"
+        table = read_flatgeobuf(url)
+        ```
+
+        Reading from a remote file with specified credentials. You can pass any `store`
+        constructed from `obstore`, including from [`S3Store`][obstore.store.S3Store],
+        [`GCSStore`][obstore.store.GCSStore], [`AzureStore`][obstore.store.AzureStore],
+        [`HTTPStore`][obstore.store.HTTPStore] or
+        [`LocalStore`][obstore.store.LocalStore].
+
+        ```py
+        from geoarrow.rust.io import read_flatgeobuf
+        from obstore.store import S3Store
+
+        store = S3Store(
+            "bucket-name",
+            access_key_id="...",
+            secret_access_key="...",
+            region="..."
+        )
+        table = read_flatgeobuf("path/in/bucket.fgb", store=store)
+        ```
 
     Returns:
         Table from FlatGeobuf file.
@@ -84,49 +100,59 @@ async def read_flatgeobuf_async(
     batch_size: int = 65536,
     bbox: Tuple[int | float, int | float, int | float, int | float] | None = None,
     coord_type: CoordTypeInput | None = None,
-    prefer_view_types: bool = True,
-    max_read_records: int | None = 1000,
+    use_view_types: bool = True,
+    max_scan_records: int | None = 1000,
     read_geometry: bool = True,
 ) -> Table:
     """
     Read a FlatGeobuf file from a url into an Arrow Table.
 
-    Example:
-
-    Reading from an HTTP(S) url:
-
-    ```py
-    from geoarrow.rust.io import read_flatgeobuf_async
-
-    url = "http://flatgeobuf.org/test/data/UScounties.fgb"
-    table = await read_flatgeobuf_async(url)
-    ```
-
-    Reading from an S3 bucket:
-
-    ```py
-    from geoarrow.rust.io import ObjectStore, read_flatgeobuf_async
-
-    options = {
-        "aws_access_key_id": "...",
-        "aws_secret_access_key": "...",
-        "aws_region": "..."
-    }
-    store = ObjectStore('s3://bucket', options=options)
-    table = await read_flatgeobuf_async("path/in/bucket.fgb", store=store)
-    ```
-
     Args:
-        path: the url or relative path to a remote FlatGeobuf file. If an argument is passed for
-            `store`, this should be a path fragment relative to the root passed to the `ObjectStore`
-            constructor.
+        path: the url or relative path to a remote FlatGeobuf file. If an argument is
+            passed for `store`, this should be a path fragment relative to the prefix of
+            the store.
 
-    Other args:
-        store: an ObjectStore instance for this url. This is required for non-HTTP urls.
+    Other Args:
+        store: an ObjectStore instance for this url. This is required only if the file
+            is at a remote location and if the store cannot be inferred.
         batch_size: the number of rows to include in each internal batch of the table.
-        bbox: A spatial filter for reading rows, of the format (minx, miny, maxx, maxy). If set to
-            `None`, no spatial filtering will be performed.
-        coord_type: The GeoArrow coordinate variant to use.
+        bbox: A spatial filter for reading rows, of the format `(minx, miny, maxx,
+            maxy)`. If set to `None`, no spatial filtering will be performed.
+        coord_type: The GeoArrow coordinate type to use for generated geometries. The default is to use "separated" coordinates.
+        use_view_types: If `True`, load string and binary columns into Arrow string view and binary view data types. These are more efficient but less widely supported than the older string and binary data types. Defaults to `True`.
+        max_scan_records: The maximum number of records to scan for schema inference. If set to `None`, all records will be scanned. Defaults to 1000.
+
+            Most FlatGeobuf files have a schema defined in the header metadata. But for
+            files that do not have a known schema, we need to scan some initial records
+            to infer a schema. Reading will fail if a new property with an unknown name
+            is found that was not in the schema. Thus, scanning fewer records will be
+            faster, but could fail later if the inferred schema was not complete.
+        read_geometry: If `True`, read the geometry column. If `False`, the geometry column will be omitted from the result. Defaults to `True`.
+
+    Examples:
+        Reading from an HTTP(S) url:
+
+        ```py
+        from geoarrow.rust.io import read_flatgeobuf_async
+
+        url = "http://flatgeobuf.org/test/data/UScounties.fgb"
+        table = await read_flatgeobuf_async(url)
+        ```
+
+        Reading from an S3 bucket:
+
+        ```py
+        from geoarrow.rust.io import ObjectStore, read_flatgeobuf_async
+        from obstore.store import S3Store
+
+        store = S3Store(
+            "bucket-name",
+            access_key_id="...",
+            secret_access_key="...",
+            region="..."
+        )
+        table = await read_flatgeobuf_async("path/in/bucket.fgb", store=store)
+        ```
 
     Returns:
         Table from FlatGeobuf file.
