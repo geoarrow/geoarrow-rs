70df71322461ce4cb7b7dc7f0bbdcfa2cf7fd9a8
Kyle Barron <kyle@developmentseed.org>
2025-08-07 13:40:08 -0400

    Add logo and favicon

--- rust/geoarrow-geo/src/lib.rs
+++ rust/geoarrow-geo/src/lib.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![doc(
+    html_logo_url = "https://github.com/geoarrow.png",
+    html_favicon_url = "https://github.com/geoarrow.png?size=32"
+)]
 
 mod area;
 mod centroid;
