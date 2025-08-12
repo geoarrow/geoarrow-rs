fe13a98287b4fd364928485fecf853dec26395cc
Kyle Barron <kyle@developmentseed.org>
2025-08-12 14:56:50 -0400

    Update crate docs

--- rust/geoarrow-geojson/src/lib.rs
+++ rust/geoarrow-geojson/src/lib.rs
@@ -1,4 +1,11 @@
+#![doc = include_str!("../README.md")]
+// #![warn(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(test), deny(unused_crate_dependencies))]
+#![doc(
+    html_logo_url = "https://github.com/geoarrow.png",
+    html_favicon_url = "https://github.com/geoarrow.png?size=32"
+)]
 
 pub mod encoder;
 pub mod writer;
