02b87e725cb276277fa5b6ba52637c8362a3f858
Kyle Barron <kyle@developmentseed.org>
2025-08-13 12:19:54 -0400

    fix compile

--- rust/geodatafusion-geoparquet/src/lib.rs
+++ rust/geodatafusion-geoparquet/src/lib.rs
@@ -1,4 +1,11 @@
+#![doc = include_str!("../README.md")]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
+// #![warn(missing_docs)]
+#![doc(
+    html_logo_url = "https://github.com/geoarrow.png",
+    html_favicon_url = "https://github.com/geoarrow.png?size=32"
+)]
 
 pub mod file_format;
 pub mod source;
@@ -9,7 +16,7 @@ mod tests {
 
     use datafusion::execution::SessionStateBuilder;
     use datafusion::prelude::SessionContext;
-    use geodatafusion::udf::native::measurement::Centroid;
+    use geodatafusion::udf::geo::processing::Centroid;
 
     use crate::file_format::GeoParquetFormatFactory;
 
