06e1d615134b249eb5fee39020673c8659978d18
Kyle Barron <kylebarron2@gmail.com>
2025-08-15 13:29:46 -0400

    feat(geoarrow-csv): add CSV writer (#1314)

--- rust/geoarrow-csv/src/lib.rs
+++ rust/geoarrow-csv/src/lib.rs
@@ -1,46 +1,11 @@
-//! Read from and write to CSV files.
-//!
-//! The CSV reader implements [`RecordBatchReader`], so you can iterate over the batches of the CSV
-//! without materializing the entire file in memory.
-//!
-//! [`RecordBatchReader`]: arrow_array::RecordBatchReader
-//!
-//! Additionally, the CSV writer takes in a [`RecordBatchReader`], so you can write an Arrow
-//! iterator to CSV without materializing all batches in memory at once.
-//!
-//! # Examples
-//!
-//! ```ignore
-//! use std::io::{Cursor, Seek};
-//!
-//! use arrow_array::RecordBatchReader;
-//!
-//! use geoarrow_schema::CoordType;
-//! use geoarrow::io::csv::{CsvReader, CsvReaderOptions};
-//! use geoarrow::table::Table;
-//!
-//! let s = r#"
-//! address,type,datetime,report location,incident number
-//! 904 7th Av,Car Fire,05/22/2019 12:55:00 PM,POINT (-122.329051 47.6069),F190051945
-//! 9610 53rd Av S,Aid Response,05/22/2019 12:55:00 PM,POINT (-122.266529 47.515984),F190051946"#;
-//!
-//! let options = CsvReaderOptions {
-//!     coord_type: CoordType::Separated,
-//!     geometry_column_name: Some("report location".to_string()),
-//!     has_header: Some(true),
-//!     ..Default::default()
-//! };
-//! let reader = CsvReader::try_new(Cursor::new(s), options).unwrap();
-//!
-//! // Now `reader` implements `arrow_array::RecordBatchReader`, so we can use TryFrom to convert
-//! // it to a geoarrow Table
-//! let table = Table::try_from(Box::new(reader) as Box<dyn arrow_array::RecordBatchReader>).unwrap();
-//! ```
-
+#![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(test), deny(unused_crate_dependencies))]
+#![doc(
+    html_logo_url = "https://github.com/geoarrow.png",
+    html_favicon_url = "https://github.com/geoarrow.png?size=32"
+)]
 
-pub use reader::{CsvReader, CsvReaderOptions};
-// pub use writer::write_csv;
-
-mod reader;
-// mod writer;
+pub mod reader;
+pub mod writer;
